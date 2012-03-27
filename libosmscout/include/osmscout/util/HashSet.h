07b443972fd615d60965a85e8afb21d3bf62493a
tteuling <tteuling@users.sourceforge.net>
2012-03-27 13:58:44 +0200

    Add header for supporting <unordered_set> and std::unordered_set.

---
+++ libosmscout/include/osmscout/util/HashSet.h
@@ -0,0 +1,40 @@
+#ifndef OSMSCOUT_UTIL_HASHSET_H
+#define OSMSCOUT_UTIL_HASHSET_H
+
+/*
+  This source is part of the libosmscout library
+  Copyright (C) 2012  Tim Teulings
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*/
+
+#include <osmscout/CoreFeatures.h>
+
+#if defined(OSMSCOUT_HAVE_UNORDERED_SET)
+  #include <unordered_set>
+#else
+  #include <set>
+#endif
+
+
+#if defined(OSMSCOUT_HAVE_UNORDERED_SET)
+  #define OSMSCOUT_HASHSET std::unordered_set
+  #define OSMSCOUT_HASHSET_HAS_RESERVE 1
+#else
+  #define OSMSCOUT_HASHMAP std::map
+  #undef OSMSCOUT_HASHMAP_HAS_RESERVE
+#endif
+
+#endif
