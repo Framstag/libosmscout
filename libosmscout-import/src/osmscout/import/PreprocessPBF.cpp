2be8e422c705d2691e619737f2c3b5c38085df7f
tteuling <tteuling@users.sourceforge.net>
2012-08-17 22:14:58 +0200

    Fixed problem with fopen 64 bit activation.

--- libosmscout-import/src/osmscout/import/PreprocessPBF.cpp
+++ libosmscout-import/src/osmscout/import/PreprocessPBF.cpp
@@ -17,9 +17,9 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 */
 
-#include <osmscout/import/PreprocessPBF.h>
-
 #include <osmscout/private/Config.h>
+
+#include <osmscout/import/PreprocessPBF.h>
 
 #include <cstdio>
 
