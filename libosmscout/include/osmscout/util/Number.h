d661a0d9b2d4d171a34018b6224897862c388e9c
tteuling <tteuling@users.sourceforge.net>
2013-04-29 21:37:39 +0200

    Updated Number.h.

--- libosmscout/include/osmscout/util/Number.h
+++ libosmscout/include/osmscout/util/Number.h
@@ -27,6 +27,7 @@
 #include <stddef.h>
 
 namespace osmscout {
+
   /**
    * Encode a signed number into the given buffer using some variable length encoding.
    *
@@ -41,8 +42,8 @@ namespace osmscout {
   inline unsigned int EncodeNumberSigned(N number,
                                          char* buffer)
   {
-    unsigned int bytes=1; // Number of bytes writte
-    char         val;     // value for current byte in buffer
+    unsigned int bytes=1;
+    char         val;
 
     if (number<0) {
       number^=static_cast<N>(-1);
@@ -62,10 +63,78 @@ namespace osmscout {
     }
 
     *buffer=val;
+    return bytes;
+  }
+
+  /**
+   * Encode an unsigned number into the given buffer using some variable length encoding.
+   *
+   * The current implementation requires the buffer to have at least space
+   * for sizeof(N)*8/7 bytes:
+   *
+   * This are 5 bytes for a 32bit value and 10 bytes for a 64bit value.
+   *
+   * The methods returns the number of bytes written.
+   */
+  template<typename N>
+  inline unsigned int EncodeNumberUnsigned(N number,
+                                           char* buffer)
+  {
+    unsigned int bytes=0;
+
+    while (number>0x7f) {
+      buffer[bytes]=static_cast<char>((number & 0x7f) | 0x80);
+      number>>=7;
+      bytes++;
+    }
+
+    buffer[bytes]=static_cast<char>(number);
+    bytes++;
 
     return bytes;
   }
 
+  template<bool is_signed, typename N>
+  struct EncodeNumberTemplated
+  {
+  };
+
+  template<typename N>
+  struct EncodeNumberTemplated<true, N>
+  {
+    static inline unsigned int f(N number,char* buffer)
+    {
+      return EncodeNumberSigned<N>(number,buffer);
+    }
+  };
+
+  template<typename N>
+  struct EncodeNumberTemplated<false, N>
+  {
+    static inline unsigned int f(N number,char* buffer)
+    {
+      return EncodeNumberUnsigned<N>(number,buffer);
+    }
+  };
+
+  /**
+   * Encode a number into the given buffer using some variable length encoding.
+   *
+   * The current implementation requires the buffer to have at least space
+   * for sizeof(N)*8/7 bytes for an unsigned number
+   * and sizeof(N)*8/7 + 1/8 bytes for a signed number
+   *
+   * This are 5 bytes for a 32bit value and 10 bytes for a64bit value.
+   *
+   * The methods returns the number of bytes written.
+   */
+  template<typename N>
+  inline unsigned int EncodeNumber(N number,
+                                   char* buffer)
+  {
+    return EncodeNumberTemplated<std::numeric_limits<N>::is_signed, N>
+      ::f(number,buffer);
+  }
 
   /**
    * Decode a signed variable length encoded number from the buffer back to
@@ -117,37 +186,6 @@ namespace osmscout {
     return bytes;
   }
 
-
-
-  /**
-   * Encode an unsigned number into the given buffer using some variable length encoding.
-   *
-   * The current implementation requires the buffer to have at least space
-   * for sizeof(N)*8/7 bytes:
-   *
-   * This are 5 bytes for a 32bit value and 10 bytes for a 64bit value.
-   *
-   * The methods returns the number of bytes written.
-   */
-  template<typename N>
-  inline unsigned int EncodeNumberUnsigned(N number,
-                                           char* buffer)
-  {
-    unsigned int bytes=0;
-
-    while (number > 0x7f) {
-      buffer[bytes]=static_cast<char>((number & 0x7f) | 0x80);
-      number>>=7;
-      bytes++;
-    }
-
-    buffer[bytes]=static_cast<char>(number);
-    bytes++;
-
-    return bytes;
-  }
-
-
   /**
    * Decode an unsigned variable length encoded number from the buffer back to
    * the variable.
@@ -164,6 +202,7 @@ namespace osmscout {
     number=0;
 
     while (true) {
+
       number|=static_cast<N>(*buffer & 0x7f) << shift;
 
       if (((*buffer) & 0x80)==0) {
@@ -178,29 +217,28 @@ namespace osmscout {
     return bytes;
   }
 
-  /**
-   * Encode a number into the given buffer using some variable length encoding.
-   *
-   * The current implementation requires the buffer to have at least space
-   * for sizeof(N)*8/7 bytes for an unsigned number
-   * and sizeof(N)*8/7 + 1/8 bytes for a signed number
-   *
-   * This are 5 bytes for a 32bit value and 10 bytes for a64bit value.
-   *
-   * The methods returns the number of bytes written.
-   */
-  template<typename N>
-  inline unsigned int EncodeNumber(N number,
-                                   char* buffer)
+  template<bool is_signed, typename N>
+  struct DecodeNumberTemplated
   {
-    if (std::numeric_limits<N>::is_signed) {
-      return EncodeNumberSigned(number,buffer);
-    }
-    else {
-      return EncodeNumberUnsigned(number,buffer);
-    }
-  }
+  };
 
+  template<typename N>
+  struct DecodeNumberTemplated<true, N>
+  {
+    static inline unsigned int f(const char* buffer, N& number)
+    {
+      return DecodeNumberSigned<N>(buffer,number);
+    }
+  };
+
+  template<typename N>
+  struct DecodeNumberTemplated<false, N>
+  {
+    static inline unsigned int f(const char* buffer, N& number)
+    {
+      return DecodeNumberUnsigned<N>(buffer,number);
+    }
+  };
 
   /**
    * Decode a variable length encoded number from the buffer back to
@@ -212,12 +250,8 @@ namespace osmscout {
   inline unsigned int DecodeNumber(const char* buffer,
                                    N& number)
   {
-    if (std::numeric_limits<N>::is_signed) {
-      return DecodeNumberSigned(buffer,number);
-    }
-    else {
-      return DecodeNumberUnsigned(buffer,number);
-    }
+    return DecodeNumberTemplated<std::numeric_limits<N>::is_signed, N>
+      ::f(buffer,number);
   }
 }
 
