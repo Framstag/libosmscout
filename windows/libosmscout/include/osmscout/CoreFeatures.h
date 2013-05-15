d2892291a3dfaa18992eb5a3af47d5e2c71474bd
tteuling <tteuling@users.sourceforge.net>
2013-05-15 21:04:05 +0200

    Tried to improve the windows specific version of Core.h and CoreFeatures.h.

--- windows/libosmscout/include/osmscout/CoreFeatures.h
+++ windows/libosmscout/include/osmscout/CoreFeatures.h
@@ -4,17 +4,9 @@
 /* libosmscout uses special gcc compiler features to export symbols */
 #define OSMSCOUT_EXPORT_SYMBOLS 1
 
-/* system header <stdint.h> is available */
+#if defined(_MSC_VER) && (_MSC_VER < 1600)
+/* system header <stdint.h> is not available */
 #undef OSMSCOUT_HAVE_STDINT_H
-
-/* system header <unordered_map> is available */
-#undef OSMSCOUT_HAVE_UNORDERED_MAP
-
-/* system header <unordered_set> is available */
-#undef OSMSCOUT_HAVE_UNORDERED_SET
-
-/* std::wstring is available */
-#define OSMSCOUT_HAVE_STD__WSTRING 1
 
 /* int8_t is available */
 #undef OSMSCOUT_HAVE_INT8_T
@@ -40,4 +32,44 @@
 /* uint64_t is available */
 #undef OSMSCOUT_HAVE_UINT64_T
 
+#else
+/* system header <stdint.h> is available */
+#define OSMSCOUT_HAVE_STDINT_H
+
+/* int8_t is available */
+#define OSMSCOUT_HAVE_INT8_T
+
+/* uint8_t is available */
+#define OSMSCOUT_HAVE_UINT8_T
+
+/* int16_t is available */
+#define OSMSCOUT_HAVE_INT16_T
+
+/* uint16_t is available */
+#define OSMSCOUT_HAVE_UINT16_T
+
+/* int32_t is available */
+#define OSMSCOUT_HAVE_INT32_T
+
+/* uint32_t is available */
+#define OSMSCOUT_HAVE_UINT32_T
+
+/* int64_t is available */
+#define OSMSCOUT_HAVE_INT64_T
+
+/* uint64_t is available */
+#define OSMSCOUT_HAVE_UINT64_T
+
+#endif
+
+/* system header <unordered_map> is available */
+#undef OSMSCOUT_HAVE_UNORDERED_MAP
+
+/* system header <unordered_set> is available */
+#undef OSMSCOUT_HAVE_UNORDERED_SET
+
+/* std::wstring is available */
+#define OSMSCOUT_HAVE_STD__WSTRING 1
+
 #endif //LIBOSMSCOUT_COREFEATURES_H
+
--- windows/libosmscout/include/osmscout/private/Config.h
+++ windows/libosmscout/include/osmscout/private/Config.h
@@ -23,9 +23,6 @@
 /* Define to 1 if you have the `mmap' function. */
 #undef HAVE_MMAP
 
-/* Define to 1 if you have the <stdint.h> header file. */
-#undef HAVE_STDINT_H
-
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
 
@@ -43,6 +40,79 @@
 
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
+
+#if defined(_MSC_VER) && (_MSC_VER < 1600)
+/* system header <stdint.h> is not available */
+#undef OSMSCOUT_HAVE_STDINT_H
+
+/* int8_t is available */
+#undef HAVE_INT8_T
+#undef OSMSCOUT_HAVE_INT8_T
+
+/* uint8_t is available */
+#undef HAVE_UINT8_T
+#undef OSMSCOUT_HAVE_UINT8_T
+
+/* int16_t is available */
+#undef OSMSCOUT_HAVE_INT16_T
+
+/* uint16_t is available */
+#undef HAVE_UINT16_T
+#undef OSMSCOUT_HAVE_UINT16_T
+
+/* int32_t is available */
+#undef HAVE_INT32_T
+#undef OSMSCOUT_HAVE_INT32_T
+
+/* uint32_t is available */
+#undef HAVE_UINT32_T
+#undef OSMSCOUT_HAVE_UINT32_T
+
+/* int64_t is available */
+#undef HAVE_INT64_T
+#undef OSMSCOUT_HAVE_INT64_T
+
+/* uint64_t is available */
+#undef HAVE_UINT64_T
+#undef OSMSCOUT_HAVE_UINT64_T
+
+#else
+/* system header <stdint.h> is available */
+#define OSMSCOUT_HAVE_STDINT_H 1
+
+/* int8_t is available */
+#define HAVE_INT8_T 1
+#define OSMSCOUT_HAVE_INT8_T 1
+
+/* uint8_t is available */
+#define HAVE_UINT8_T 1
+#define OSMSCOUT_HAVE_UINT8_T 1
+
+/* int16_t is available */
+#define HAVE_INT16_T 1
+#define OSMSCOUT_HAVE_INT16_T 1
+
+/* uint16_t is available */
+#define HAVE_UINT16_T 1
+#define OSMSCOUT_HAVE_UINT16_T 1
+
+/* int32_t is available */
+#define HAVE_INT32_T 1
+#define OSMSCOUT_HAVE_INT32_T 1
+
+/* uint32_t is available */
+#define HAVE_UINT32_T 1
+#define OSMSCOUT_HAVE_UINT32_T 1
+
+/* int64_t is available */
+#define HAVE_INT64_T 1
+#define OSMSCOUT_HAVE_INT64_T 1
+
+/* uint64_t is available */
+#define HAVE_UINT64_T 1
+#define OSMSCOUT_HAVE_UINT64_T 1
+
+#endif
 
 /* Define to 1 if you have the <unistd.h> header file. */
 #undef HAVE_UNISTD_H
