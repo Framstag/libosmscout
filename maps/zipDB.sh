29f6a36a4b7a8095f1a0bda5a6260f407b554e1b
tteuling <tteuling@users.sourceforge.net>
2014-06-14 23:25:39 +0200

    Added little script to zip the mobile relevant database files. This allows us to check the compression rate and search for files with possible optiomizations (compression is high).

---
+++ maps/zipDB.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+  echo "zipDB.sh <map directory>"
+  exit 1
+fi
+
+scriptDirectory="${BASH_SOURCE%/*}"
+if [[ ! -d "$scriptDirectory" ]]; then scriptDirectory="$PWD"; fi
+
+mapDirectory="$1"
+
+if [ ! -d "$mapDirectory" ]; then
+  echo "Map directory $mapDirectory does not exist!"
+  exit 1
+fi
+
+outputFile="$scriptDirectory/$mapDirectory.zip"
+
+zip -u -9 -du \
+    $outputFile \
+    "$mapDirectory/types.dat" \
+    "$mapDirectory/bounding.dat" \
+    "$mapDirectory/nodes.dat" \
+    "$mapDirectory/areas.dat" \
+    "$mapDirectory/ways.dat" \
+    "$mapDirectory/areanode.idx" \
+    "$mapDirectory/areaarea.idx" \
+    "$mapDirectory/areaway.idx" \
+    "$mapDirectory/areasopt.dat" \
+    "$mapDirectory/waysopt.dat" \
+    "$mapDirectory/location.idx" \
+    "$mapDirectory/water.idx" \
+    "$mapDirectory/intersections.dat" \
+    "$mapDirectory/intersections.idx" \
+    "$mapDirectory/routefoot.dat" \
+    "$mapDirectory/routefoot.idx" \
+    "$mapDirectory/routebicycle.dat" \
+    "$mapDirectory/routebicycle.idx" \
+    "$mapDirectory/routecar.dat" \
+    "$mapDirectory/routecar.idx"
+    
