2fce284eec970701c0c311380552f74077892761
tteuling <tteuling@users.sourceforge.net>
2015-03-29 13:31:42 +0200

    Updated ax_openmp.m4 macro. Added new visibility.m4 macro, replacing the custom test made before. Exchange autogen.sh with direct call to autoreconf and updated comand line parameters. Exchanged some obsoleted internal autoconf m4 macros call with the current version, removing deprication warnings during autoreconf.

--- libosmscout-map-agg/autogen.sh
+++ libosmscout-map-agg/autogen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-autoreconf --force --install -I m4 -Woverride -Wportability -Wsyntax -Wunsupported
+autoreconf --force --install -I m4 -Woverride -Wportability -Wsyntax -Wunsupported -Wobsolete
