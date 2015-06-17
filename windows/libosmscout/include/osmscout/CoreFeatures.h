44c9e785df278934888925899d8fc13cca1d3957
tteuling <tteuling@users.sourceforge.net>
2015-06-17 22:37:18 +0200

    Updated Microsoft projects files for libosmscout, libosmscout-import and Import to MSVC 2013.

--- windows/libosmscout/include/osmscout/CoreFeatures.h
+++ windows/libosmscout/include/osmscout/CoreFeatures.h
@@ -2,74 +2,66 @@
 #define LIBOSMSCOUT_COREFEATURES_H
 
 /* libosmscout uses special gcc compiler features to export symbols */
-#define OSMSCOUT_EXPORT_SYMBOLS 1
+/* #undef OSMSCOUT_EXPORT_SYMBOLS */
 
-#if defined(_MSC_VER) && (_MSC_VER < 1600)
-/* system header <stdint.h> is not available */
-#undef OSMSCOUT_HAVE_STDINT_H
-
-/* int8_t is available */
-#undef OSMSCOUT_HAVE_INT8_T
-
-/* uint8_t is available */
-#undef OSMSCOUT_HAVE_UINT8_T
-
-/* int16_t is available */
-#undef OSMSCOUT_HAVE_INT16_T
-
-/* uint16_t is available */
-#undef OSMSCOUT_HAVE_UINT16_T
-
-/* int32_t is available */
-#undef OSMSCOUT_HAVE_INT32_T
-
-/* uint32_t is available */
-#undef OSMSCOUT_HAVE_UINT32_T
-
-/* int64_t is available */
-#undef OSMSCOUT_HAVE_INT64_T
-
-/* uint64_t is available */
-#undef OSMSCOUT_HAVE_UINT64_T
-
-#else
 /* system header <stdint.h> is available */
-#define OSMSCOUT_HAVE_STDINT_H
-
-/* int8_t is available */
-#define OSMSCOUT_HAVE_INT8_T
-
-/* uint8_t is available */
-#define OSMSCOUT_HAVE_UINT8_T
-
-/* int16_t is available */
-#define OSMSCOUT_HAVE_INT16_T
-
-/* uint16_t is available */
-#define OSMSCOUT_HAVE_UINT16_T
-
-/* int32_t is available */
-#define OSMSCOUT_HAVE_INT32_T
-
-/* uint32_t is available */
-#define OSMSCOUT_HAVE_UINT32_T
-
-/* int64_t is available */
-#define OSMSCOUT_HAVE_INT64_T
-
-/* uint64_t is available */
-#define OSMSCOUT_HAVE_UINT64_T
-
-#endif
-
-/* system header <unordered_map> is available */
-#undef OSMSCOUT_HAVE_UNORDERED_MAP
-
-/* system header <unordered_set> is available */
-#undef OSMSCOUT_HAVE_UNORDERED_SET
+#define OSMSCOUT_HAVE_STDINT_H 1
 
 /* std::wstring is available */
-#define OSMSCOUT_HAVE_STD__WSTRING 1
+#define OSMSCOUT_HAVE_STD_WSTRING 1
 
-#endif //LIBOSMSCOUT_COREFEATURES_H
+/* int8_t is available */
+#define OSMSCOUT_HAVE_INT8_T 1
 
+/* uint8_t is available */
+#define OSMSCOUT_HAVE_UINT8_T 1
+
+/* int16_t is available */
+#define OSMSCOUT_HAVE_INT16_T 1
+
+/* uint16_t is available */
+#define OSMSCOUT_HAVE_UINT16_T 1
+
+/* int32_t is available */
+#define OSMSCOUT_HAVE_INT32_T 1
+
+/* uint32_t is available */
+#define OSMSCOUT_HAVE_UINT32_T 1
+
+/* int64_t is available */
+#define OSMSCOUT_HAVE_INT64_T 1
+
+/* uint64_t is available */
+#define OSMSCOUT_HAVE_UINT64_T 1
+
+/* SSE2 processor extension available */
+#define OSMSCOUT_HAVE_SSE2 1
+
+/* libosmscout needs to include <assert.h> */
+/* #undef OSMSCOUT_REQUIRES_ASSERTH */
+
+/* libosmscout needs to include <math.h> */
+#define OSMSCOUT_REQUIRES_MATHH 1
+
+/* math function log2(double) is available */
+#define OSMSCOUT_HAVE_LOG2 1
+
+/* math function atanh(double) is available */
+#define OSMSCOUT_HAVE_ATANH 1
+
+/* math function lround(double) is available */
+#define OSMSCOUT_HAVE_LROUND 1
+
+/* system header <thread> is available */
+#define OSMSCOUT_HAVE_THREAD 1
+
+/* libmarisa is available */
+/* #undef OSMSCOUT_HAVE_LIB_MARISA 1 */
+
+/* long long is available */
+#define OSMSCOUT_HAVE_LONG_LONG 1
+
+/* unsigned long long is available */
+#define OSMSCOUT_HAVE_ULONG_LONG 1
+
+#endif
--- windows/libosmscout/include/osmscout/private/Config.h
+++ windows/libosmscout/include/osmscout/private/Config.h
@@ -1,30 +1,80 @@
-#ifndef LIBOSMSCOUT_CONFIG_H
-#define LIBOSMSCOUT_CONFIG_H
-/* include/osmscout/private/Config.h.  Generated from Config.h.in by configure.  */
-/* include/osmscout/private/Config.h.in.  Generated from configure.ac by autoheader.  */
+#ifndef LIBOSMSCOUT_PRIVATE_CONFIG_H
+#define LIBOSMSCOUT_PRIVATE CONFIG_H
 
-/* Define to 1 if you have the declaration of `atanh(double)', and to 0 if you
-   don't. */
-#undef HAVE_DECL_ATANH
+/* Support Altivec instructions */
+/* #undef HAVE_ALTIVEC */
 
-/* Define to 1 if you have the declaration of `log2(double)', and to 0 if you
-   don't. */
-#undef HAVE_DECL_LOG2
+/* Support AVX (Advanced Vector Extensions) instructions */
+/* #undef HAVE_AVX */
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#define HAVE_DLFCN_H 1
+/* #undef HAVE_DLFCN_H 1 */
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+/* #undef HAVE_FCNTL_H 1 */
+
+/* Define to 1 if fseeko (and presumably ftello) exists and is declared. */
+/* #undef HAVE_FSEEKO */
+
+/* Define to 1 if the system has the type `int16_t'. */
+#define HAVE_INT16_T 1
+
+/* Define to 1 if the system has the type `int32_t'. */
+#define HAVE_INT32_T 1
+
+/* Define to 1 if the system has the type `int64_t'. */
+#define HAVE_INT64_T 1
+
+/* Define to 1 if the system has the type `int8_t'. */
+#define HAVE_INT8_T 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
+
+/* Define to 1 if the system has the type `long long'. */
+#define HAVE_LONG_LONG 1
 
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the `mmap' function. */
-#undef HAVE_MMAP
+/* #undef HAVE_MMAP 1 */
+
+/* Support mmx instructions */
+/* #undef HAVE_MMX */
+
+/* Define to 1 if you have the `posix_fadvise' function. */
+/* #undef HAVE_POSIX_FADVISE 1 */
+
+/* Define to 1 if you have the `posix_madvise' function. */
+/* #undef HAVE_POSIX_MADVISE 1 */
+
+/* Support SSE (Streaming SIMD Extensions) instructions */
+/* #undefi HAVE_SSE */
+
+/* Support SSE2 (Streaming SIMD Extensions 2) instructions */
+/* #undef HAVE_SSE2 */
+
+/* Support SSE3 (Streaming SIMD Extensions 3) instructions */
+/* #undef HAVE_SSE3 */
+
+/* Support SSSE4.1 (Streaming SIMD Extensions 4.1) instructions */
+/* #undef HAVE_SSE4_1 */
+
+/* Support SSSE4.2 (Streaming SIMD Extensions 4.2) instructions */
+/* #undef HAVE_SSE4_2 /*/
+
+/* Support SSSE3 (Supplemental Streaming SIMD Extensions 3) instructions */
+/* #undef HAVE_SSSE3 */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
+
+/* Define to 1 if the system has the type `std::wstring'. */
+#define HAVE_STD__WSTRING 1
 
 /* Define to 1 if you have the <strings.h> header file. */
 #define HAVE_STRINGS_H 1
@@ -36,102 +86,104 @@
 #define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/time.h> header file. */
-#undef HAVE_SYS_TIME_H
+/* #undef HAVE_SYS_TIME_H */
 
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
 
-#if defined(_MSC_VER) && (_MSC_VER < 1600)
-/* system header <stdint.h> is not available */
-#undef OSMSCOUT_HAVE_STDINT_H
+/* Define to 1 if you have the <thread> header file. */
+#define HAVE_THREAD 1
 
-/* int8_t is available */
-#undef HAVE_INT8_T
-#undef OSMSCOUT_HAVE_INT8_T
+/* Define to 1 if the system has the type `uint16_t'. */
+#define HAVE_UINT16_T 1
 
-/* uint8_t is available */
-#undef HAVE_UINT8_T
-#undef OSMSCOUT_HAVE_UINT8_T
+/* Define to 1 if the system has the type `uint32_t'. */
+#define HAVE_UINT32_T 1
+
+/* Define to 1 if the system has the type `uint64_t'. */
+#define HAVE_UINT64_T 1
+
+/* Define to 1 if the system has the type `uint8_t'. */
+#define HAVE_UINT8_T 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if the system has the type `unsigned long long'. */
+#define HAVE_UNSIGNED_LONG_LONG 1
+
+/* Define to 1 or 0, depending whether the compiler supports simple visibility
+   declarations. */
+#define HAVE_VISIBILITY 1
+
+/* libosmscout uses special gcc compiler features to export symbols */
+/* #undef OSMSCOUT_EXPORT_SYMBOLS */
+
+/* math function atanh(double) is available */
+#define OSMSCOUT_HAVE_ATANH 1
+
+/* standard library has support for atomic */
+#define OSMSCOUT_HAVE_ATOMIC 1
 
 /* int16_t is available */
-#undef OSMSCOUT_HAVE_INT16_T
-
-/* uint16_t is available */
-#undef HAVE_UINT16_T
-#undef OSMSCOUT_HAVE_UINT16_T
+#define OSMSCOUT_HAVE_INT16_T 1
 
 /* int32_t is available */
-#undef HAVE_INT32_T
-#undef OSMSCOUT_HAVE_INT32_T
-
-/* uint32_t is available */
-#undef HAVE_UINT32_T
-#undef OSMSCOUT_HAVE_UINT32_T
+#define OSMSCOUT_HAVE_INT32_T 1
 
 /* int64_t is available */
-#undef HAVE_INT64_T
-#undef OSMSCOUT_HAVE_INT64_T
+#define OSMSCOUT_HAVE_INT64_T 1
 
-/* uint64_t is available */
-#undef HAVE_UINT64_T
-#undef OSMSCOUT_HAVE_UINT64_T
+/* int8_t is available */
+#define OSMSCOUT_HAVE_INT8_T 1
 
-#else
+/* libmarisa detected */
+/* #undef OSMSCOUT_HAVE_LIB_MARISA */
+
+/* math function log2(double) is available */
+#define OSMSCOUT_HAVE_LOG2 1
+
+/* long long is available */
+#define OSMSCOUT_HAVE_LONG_LONG 1
+
+/* math function lround(double) is available */
+#define OSMSCOUT_HAVE_LROUND 1
+
+/* standard library has support for mutex */
+#define OSMSCOUT_HAVE_MUTEX 1
+
+/* SSE2 processor extension available */
+#define OSMSCOUT_HAVE_SSE2 1
+
 /* system header <stdint.h> is available */
 #define OSMSCOUT_HAVE_STDINT_H 1
 
-/* int8_t is available */
-#define HAVE_INT8_T 1
-#define OSMSCOUT_HAVE_INT8_T 1
+/* std::wstring is available */
+#define OSMSCOUT_HAVE_STD_WSTRING 1
 
-/* uint8_t is available */
-#define HAVE_UINT8_T 1
-#define OSMSCOUT_HAVE_UINT8_T 1
-
-/* int16_t is available */
-#define HAVE_INT16_T 1
-#define OSMSCOUT_HAVE_INT16_T 1
+/* system header <thread> is available */
+#define OSMSCOUT_HAVE_THREAD 1
 
 /* uint16_t is available */
-#define HAVE_UINT16_T 1
 #define OSMSCOUT_HAVE_UINT16_T 1
 
-/* int32_t is available */
-#define HAVE_INT32_T 1
-#define OSMSCOUT_HAVE_INT32_T 1
-
 /* uint32_t is available */
-#define HAVE_UINT32_T 1
 #define OSMSCOUT_HAVE_UINT32_T 1
 
-/* int64_t is available */
-#define HAVE_INT64_T 1
-#define OSMSCOUT_HAVE_INT64_T 1
-
 /* uint64_t is available */
-#define HAVE_UINT64_T 1
 #define OSMSCOUT_HAVE_UINT64_T 1
 
-#endif
+/* uint8_t is available */
+#define OSMSCOUT_HAVE_UINT8_T 1
 
-/* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+/* unsigned long long is available */
+#define OSMSCOUT_HAVE_ULONG_LONG 1
 
-/* Define to 1 if you have the <unordered_map> header file. */
-#define HAVE_UNORDERED_MAP 0
+/* libosmscout needs to include <assert.h> */
+/* #undef OSMSCOUT_REQUIRES_ASSERTH */
 
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
-   */
-#define LT_OBJDIR ".libs/"
-
-/* libosmscout uses special gcc compiler features to export symbols */
-#define OSMSCOUT_EXPORT_SYMBOLS 1
-
-/* system header <stdint.h> is available */
-#undef OSMSCOUT_HAVE_STDINT_H
-
-/* system header <unordered_map> is available */
-#undef OSMSCOUT_HAVE_UNORDERED_MAP
+/* libosmscout needs to include <math.h> */
+#define OSMSCOUT_REQUIRES_MATHH 1
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT "tim@teulings.org"
@@ -157,11 +209,4 @@
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
-/* Define to `unsigned int' if <sys/types.h> does not define. */
-/* #undef size_t */
-
-/* Enable SSE Math */
-/* dont use "#if _M_IX86_FP >= 2" since that can mess up templats in projects using this library which compile without this flag */
-#define USE_SSE2_MATH
-
-#endif LIBOSMSCOUT_CONFIG_H
+#endif
