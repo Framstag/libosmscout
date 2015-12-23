15fd8031337e21819e24d33d29cee6a3fa26e88e
Tim Teulings <tim@framstag.com>
2015-12-23 08:26:35 +0100

    Try to solve quoting problem.

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -10,7 +10,7 @@ echo Bits: %BIT%
 
 IF %COMPILER%==msys2 (
   @echo on
-  SET PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%
+  SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
   bash -lc "update-core"
   bash -lc "pacman -S --needed --noconfirm git autoconf automake make mingw-w64-x86_64-toolchain mingw-w64-x86_64-libtool mingw-w64-x86_64-protobuf mingw-w64-x86_64-libxml2 mingw-w64-x86_64-cairo mingw-w64-x86_64-pango mingw-w64-x86_64-qt5"
 )
