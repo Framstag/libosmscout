4b28ede77588bf7bd90458e4809786035e2d1c73
Tim Teulings <tim@framstag.com>
2015-12-27 09:06:45 +0100

    Better handle nested IF blocks in script.

--- ci/appveyor/build.bat
+++ ci/appveyor/build.bat
@@ -4,8 +4,10 @@ cd %APPVEYOR_BUILD_FOLDER%
 
 echo Compiler: %COMPILER%
 echo Architecture: %MSYS2_ARCH%
+echo Platform: %PLATFORM%
 echo MSYS2 directory: %MSYS2_DIR%
 echo MSYS2 system: %MSYSTEM%
+echo Configuration: %CONFIGURATION%
 echo Bits: %BIT%
 
 IF %COMPILER%==msys2 (
