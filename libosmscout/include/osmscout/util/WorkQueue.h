dc5db204606851c24b3fe9c4199e692361a58c20
tteuling <tteuling@users.sourceforge.net>
2015-12-30 19:57:12 +0100

    Added new WorkQueue template.

---
+++ libosmscout/include/osmscout/util/WorkQueue.h
@@ -0,0 +1,106 @@
+#ifndef OSMSCOUT_UTIL_WORKQUEUE_H
+#define OSMSCOUT_UTIL_WORKQUEUE_H
+
+/*
+  This source is part of the libosmscout library
+  Copyright (C) 2015 Tim Teulings
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*/
+
+#include <condition_variable>
+#include <deque>
+#include <future>
+#include <memory>
+#include <thread>
+
+#include <osmscout/private/CoreImportExport.h>
+
+namespace osmscout {
+
+  template<class R>
+  class OSMSCOUT_API WorkQueue
+  {
+  private:
+    typedef std::packaged_task<R()> Task;
+
+  private:
+    std::mutex              mutex;
+    std::condition_variable condition;
+    std::deque<Task>        tasks;
+    bool                    running;
+
+  public:
+    WorkQueue();
+    ~WorkQueue();
+
+    void PushTask(Task& task);
+    bool PopTask(Task& task);
+
+    void Stop();
+  };
+
+  template<class R>
+  WorkQueue<R>::WorkQueue()
+  : running(true)
+  {
+    // no code
+  }
+
+  template<class R>
+  WorkQueue<R>::~WorkQueue()
+  {
+    // no code
+  }
+
+  template<class R>
+  void WorkQueue<R>::PushTask(Task& task)
+  {
+    std::lock_guard<std::mutex> lock(mutex);
+
+    tasks.push_back(std::move(task));
+
+    condition.notify_one();
+  }
+
+  template<class R>
+  bool WorkQueue<R>::PopTask(Task& task)
+  {
+    std::unique_lock<std::mutex> lock(mutex);
+
+    condition.wait(lock,[this]{return !tasks.empty() || !running;});
+
+    if (!running) {
+      return false;
+    }
+
+    task=std::move(tasks.front());
+    tasks.pop_front();
+
+    return true;
+  }
+
+  template<class R>
+  void WorkQueue<R>::Stop()
+  {
+    std::lock_guard<std::mutex> lock(mutex);
+
+    running=false;
+
+    condition.notify_all();
+  }
+}
+
+#endif
