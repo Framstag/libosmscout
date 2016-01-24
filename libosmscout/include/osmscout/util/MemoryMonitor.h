0965c5348522062f76a08c86451f41d9e9703ba6
tteuling <tteuling@users.sourceforge.net>
2016-01-24 11:14:01 +0100

    New util class MemoryMonitor for background monitoring of memory consumption.

---
+++ libosmscout/include/osmscout/util/MemoryMonitor.h
@@ -0,0 +1,65 @@
+#ifndef OSMSCOUT_UTIL_MEMORYMONITOR_H
+#define OSMSCOUT_UTIL_MEMORYMONITOR_H
+
+/*
+  This source is part of the libosmscout library
+  Copyright (C) 2016 Tim Teulings
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*/
+
+#include <atomic>
+#include <mutex>
+#include <thread>
+
+#include <osmscout/private/CoreImportExport.h>
+
+namespace osmscout {
+
+  /**
+   * Starts a background threads that checks every 200ms the current memory usage of the current
+   * process and accumulates it to return the maximum memory usage since start or reset.
+   *
+   * Thread is started on construction and join again on destruction (with up to 200ms wait time).
+   *
+   * Implementation is OS specific, if GetValue() retutns 0.0 for each value there is likely no
+   * implementation for your OS.
+   */
+  class OSMSCOUT_API MemoryMonitor
+  {
+  private:
+    std::atomic<bool> quit;
+    std::mutex        mutex;
+    double            maxVMUsage;
+    double            maxResidentSet;
+    std::thread       thread;
+
+  private:
+    void SignalStop();
+    void BackgroundJob();
+
+  public:
+    MemoryMonitor();
+    ~MemoryMonitor();
+
+    void GetMaxValue(double& vmUsage,
+                     double& residentSet);
+
+    void Reset();
+  };
+
+}
+
+#endif
---
+++ libosmscout/src/osmscout/util/MemoryMonitor.cpp
@@ -0,0 +1,115 @@
+/*
+  This source is part of the libosmscout library
+  Copyright (C) 2016 Tim Teulings
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*/
+
+#include <osmscout/util/MemoryMonitor.h>
+
+// For MemoryMonitor
+#ifdef __linux__
+#include <unistd.h>
+
+#include <fstream>
+#endif
+
+namespace osmscout {
+
+  MemoryMonitor::MemoryMonitor()
+  : quit(false),
+    maxVMUsage(0.0),
+    maxResidentSet(0.0),
+    thread(&MemoryMonitor::BackgroundJob,this)
+  {
+    // no code
+  }
+
+  MemoryMonitor::~MemoryMonitor()
+  {
+    SignalStop();
+    thread.join();
+  }
+
+  /**
+   * The actual background thread, sleeping for 200ms.
+   */
+  void MemoryMonitor::BackgroundJob()
+  {
+    while (!quit) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+      {
+        std::lock_guard<std::mutex> lock(mutex);
+      }
+    }
+  }
+  /**
+   * Sinal the backgound thread to stop.
+   */
+  void MemoryMonitor::SignalStop()
+  {
+    quit=true;
+  }
+
+  /**
+   * Return the maximum measured memory usage. If there is no implementation
+   * for your OS, both values return are 0.0.
+   */
+  void MemoryMonitor::GetMaxValue(double& vmUsage,
+                                  double& residentSet)
+  {
+    std::lock_guard<std::mutex> lock(mutex);
+
+    double currentVMUsage=0.0;
+    double currentResidentSet=0.0;
+
+#ifdef __linux__
+    double vsize;
+    double rss;
+    {
+      std::ifstream ifs("/proc/self/statm", std::ios_base::in);
+
+      ifs >> vsize >> rss;
+    }
+
+    long pageSizeInByte=sysconf(_SC_PAGE_SIZE);
+
+    currentVMUsage=vsize*pageSizeInByte;
+    currentResidentSet=rss*pageSizeInByte;
+#else
+    this->vmUsage=0.0;
+    this->residentSet=0.0;
+#endif
+
+    maxVMUsage=std::max(maxVMUsage,currentVMUsage);
+    maxResidentSet=std::max(maxResidentSet,currentResidentSet);
+
+    vmUsage=maxVMUsage;
+    residentSet=maxResidentSet;
+  }
+
+  /**
+   * Resets the internal values to 0.0.
+   */
+  void MemoryMonitor::Reset()
+  {
+    std::lock_guard<std::mutex> lock(mutex);
+
+    maxVMUsage=0.0;
+    maxResidentSet=0.0;
+  }
+}
+
