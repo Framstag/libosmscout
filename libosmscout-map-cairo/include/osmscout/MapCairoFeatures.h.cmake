94233e570cbbafb2dacff8839df811a989218a97
Transporter <Ogre.Transporter@gmail.com>
2016-03-01 13:49:04 +0100

    Central configuration files

--- libosmscout-map-cairo/include/osmscout/MapCairoFeatures.h.cmake
+++ libosmscout-map-cairo/include/osmscout/MapCairoFeatures.h.cmake
@@ -1,7 +1,9 @@
 #ifndef LIBOSMSCOUT_MAP_CAIRO_FEATURES
 #define LIBOSMSCOUT_MAP_CAIRO_FEATURES
 
+#ifndef OSMSCOUT_MAP_CAIRO_HAVE_LIB_PANGO
 /* The cairo backend can make use of pango */
 #cmakedefine OSMSCOUT_MAP_CAIRO_HAVE_LIB_PANGO
+#endif
 
 #endif
--- libosmscout-map-svg/include/osmscout/MapSVGFeatures.h.cmake
+++ libosmscout-map-svg/include/osmscout/MapSVGFeatures.h.cmake
@@ -1,7 +1,9 @@
 #ifndef LIBOSMSCOUT_MAP_SVG_FEATURES
 #define LIBOSMSCOUT_MAP_SVG_FEATURES
 
+#ifndef OSMSCOUT_MAP_SVG_HAVE_LIB_PANGO
 /* libpango found */
 #cmakedefine OSMSCOUT_MAP_SVG_HAVE_LIB_PANGO
+#endif
 
 #endif
