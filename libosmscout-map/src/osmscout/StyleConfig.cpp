9afb01ae8350e6b95865c11b5731e97f331f384a
tteuling <tteuling@users.sourceforge.net>
2016-04-06 20:44:48 +0200

    Refactored code.

--- libosmscout-map/src/osmscout/StyleConfig.cpp
+++ libosmscout-map/src/osmscout/StyleConfig.cpp
@@ -2559,21 +2559,20 @@ namespace osmscout {
       success=!parser->errors->hasErrors;
 
       errors.clear();
-      if(!success){
-	for(std::list<oss::Errors::Err>::iterator it=parser->errors->errors.begin(); it != parser->errors->errors.end(); ++it){
-	  oss::Errors::Err err = *it;
-	  switch(err.type){
-	  case oss::Errors::Err::Symbol: 
-	    errors.push_back(std::to_string(err.line)+","+std::to_string(err.column)+std::string(" Symbol:")+err.text);
+      if (!success) {
+        for (const auto& err : parser->errors->errors) {
+          switch(err.type) {
+          case oss::Errors::Err::Symbol:
+            errors.push_back(std::to_string(err.line)+","+std::to_string(err.column)+std::string(" Symbol:")+err.text);
             break;
-	  case oss::Errors::Err::Error: 
-	    errors.push_back(std::to_string(err.line)+","+std::to_string(err.column)+std::string(" Error:")+err.text);
+          case oss::Errors::Err::Error:
+            errors.push_back(std::to_string(err.line)+","+std::to_string(err.column)+std::string(" Error:")+err.text);
             break;
-	  case oss::Errors::Err::Warning: 
-	    errors.push_back(std::to_string(err.line)+","+std::to_string(err.column)+std::string(" Warning:")+err.text);
+          case oss::Errors::Err::Warning:
+            errors.push_back(std::to_string(err.line)+","+std::to_string(err.column)+std::string(" Warning:")+err.text);
             break;
-	  case oss::Errors::Err::Exception: 
-	    errors.push_back(std::to_string(err.line)+","+std::to_string(err.column)+std::string(" Exception:")+err.text);
+          case oss::Errors::Err::Exception:
+            errors.push_back(std::to_string(err.line)+","+std::to_string(err.column)+std::string(" Exception:")+err.text);
             break;
           default:
             break;
