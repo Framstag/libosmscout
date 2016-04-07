a53ab2b770a11e83f401b0a985aacb096769286c
tteuling <tteuling@users.sourceforge.net>
2016-04-07 20:17:44 +0200

    Added support for cmake under appveyor.

--- ci/appveyor/build.bat
+++ ci/appveyor/build.bat
@@ -13,26 +13,39 @@ echo Bits: %BIT%
 IF %COMPILER%==msys2 (
   @echo on
   SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
-  bash -lc "cd ${APPVEYOR_BUILD_FOLDER} && . setupMSYS2.sh && exec 0</dev/null && make full"
+  IF %BUILDTOOL%==autoconf (
+    bash -lc "cd ${APPVEYOR_BUILD_FOLDER} && . setupMSYS2.sh && exec 0</dev/null && make full"
+  )
+  ELSE (
+    bash -lc "cd ${APPVEYOR_BUILD_FOLDER} && . setupMSYS2.sh && exec 0</dev/null && mkdir build && cd build && cmake .. && make"
+  )  
 )
 
 IF %COMPILER%==msvc2015 (
   @echo on
-  copy windows\libosmscout\include\osmscout\CoreFeatures.h libosmscout\include\osmscout\CoreFeatures.h 
-  copy windows\libosmscout\include\osmscout\private\Config.h  libosmscout\include\osmscout\private\Config.h 
-  copy windows\libosmscout\msvc2015_libosmscout.vcxproj libosmscout\libosmscout.vcxproj
+  IF %BUILDTOOL%==msbuild (
+    copy windows\libosmscout\include\osmscout\CoreFeatures.h libosmscout\include\osmscout\CoreFeatures.h 
+    copy windows\libosmscout\include\osmscout\private\Config.h  libosmscout\include\osmscout\private\Config.h 
+    copy windows\libosmscout\msvc2015_libosmscout.vcxproj libosmscout\libosmscout.vcxproj
 
-  copy windows\libosmscout-import\include\osmscout\ImportFeatures.h libosmscout-import\include\osmscout\ImportFeatures.h 
-  copy windows\libosmscout-import\include\osmscout\private\Config.h libosmscout-import\include\osmscout\private\Config.h 
-  copy windows\libosmscout-import\msvc2015_libosmscout-import.vcxproj libosmscout-import\libosmscout-import.vcxproj 
+    copy windows\libosmscout-import\include\osmscout\ImportFeatures.h libosmscout-import\include\osmscout\ImportFeatures.h 
+    copy windows\libosmscout-import\include\osmscout\private\Config.h libosmscout-import\include\osmscout\private\Config.h 
+    copy windows\libosmscout-import\msvc2015_libosmscout-import.vcxproj libosmscout-import\libosmscout-import.vcxproj 
 
-  copy windows\libosmscout-map\include\osmscout\MapFeatures.h libosmscout-map\include\osmscout\MapFeatures.h 
-  copy windows\libosmscout-map\include\osmscout\private\Config.h  libosmscout-map\include\osmscout\private\Config.h 
-  copy windows\libosmscout-map\msvc2015_libosmscout-map.vcxproj libosmscout-map\libosmscout-map.vcxproj 
+    copy windows\libosmscout-map\include\osmscout\MapFeatures.h libosmscout-map\include\osmscout\MapFeatures.h 
+    copy windows\libosmscout-map\include\osmscout\private\Config.h  libosmscout-map\include\osmscout\private\Config.h 
+    copy windows\libosmscout-map\msvc2015_libosmscout-map.vcxproj libosmscout-map\libosmscout-map.vcxproj 
 
-  copy windows\Import\msvc2015_Import.vcxproj Import\Import.vcxproj
+    copy windows\Import\msvc2015_Import.vcxproj Import\Import.vcxproj
 
-  copy windows\msvc2015_libosmscout.sln libosmscout.sln   
+    copy windows\msvc2015_libosmscout.sln libosmscout.sln   
 
-  "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% libosmscout.sln   
+    "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% libosmscout.sln   
+  )
+  ELSE (
+    mkdir build
+    cd build
+    cmake .. -G "Visual Studio 14 2015 Win64"
+    cmake --build .
+  )
 )
--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -14,9 +14,15 @@ IF %COMPILER%==msys2 (
   @echo on
   SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
   bash -lc "pacman --noconfirm --sync pacman-mirrors"
-REM  bash -lc "exec 0</dev/null && update-core"
-REM  bash -lc "pacman -Su --noconfirm"
+  bash -lc "pacman -S --needed --noconfirm git"
+  
+  IF %BUILDTOOL%==autoconf (
   bash -lc "pacman -S --needed --noconfirm git autoconf automake make"
+  )
+  ELSE (
+  bash -lc "pacman -S --needed --noconfirm git make cmake"
+  )
+  
   bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-toolchain mingw-w64-x86_64-libtool mingw-w64-x86_64-protobuf mingw-w64-x86_64-libxml2 mingw-w64-x86_64-cairo mingw-w64-x86_64-pango mingw-w64-x86_64-qt5 mingw-w64-x86_64-ccache"
 )
 
@@ -24,14 +30,13 @@ IF %COMPILER%==msvc2015 (
 cinst wget -x86
 cinst 7zip.commandline -x86
 
-IF %PLATFORM%==x64 (
-wget -q ftp://ftp.zlatkovic.com/libxml/64bit/zlib-1.2.8-win32-x86_64.7z -O zlib-1.2.8-win32-x86_64.7z
-wget -q ftp://ftp.zlatkovic.com/libxml/64bit/iconv-1.14-win32-x86_64.7z -O iconv-1.14-win32-x86_64.7z
-wget -q ftp://ftp.zlatkovic.com/libxml/64bit/libxml2-2.9.3-win32-x86_64.7z -O libxml2-2.9.3-win32-x86_64.7z
+  IF %PLATFORM%==x64 (
+  wget -q ftp://ftp.zlatkovic.com/libxml/64bit/zlib-1.2.8-win32-x86_64.7z -O zlib-1.2.8-win32-x86_64.7z
+  wget -q ftp://ftp.zlatkovic.com/libxml/64bit/iconv-1.14-win32-x86_64.7z -O iconv-1.14-win32-x86_64.7z
+  wget -q ftp://ftp.zlatkovic.com/libxml/64bit/libxml2-2.9.3-win32-x86_64.7z -O libxml2-2.9.3-win32-x86_64.7z
 
-7z x zlib-1.2.8-win32-x86_64.7z -ozlib -y > nul
-7z x iconv-1.14-win32-x86_64.7z -oiconv -y > nul
-7z x libxml2-2.9.3-win32-x86_64.7z -olibxml2 -y > nul
-)
-
+  7z x zlib-1.2.8-win32-x86_64.7z -ozlib -y > nul
+  7z x iconv-1.14-win32-x86_64.7z -oiconv -y > nul
+  7z x libxml2-2.9.3-win32-x86_64.7z -olibxml2 -y > nul
+  )
 )
