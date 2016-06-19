88bd4fb295afd50d869dd7a48e783516a386f194
tteuling <tteuling@users.sourceforge.net>
2016-06-19 21:29:39 +0200

    Try to get package installation on Travis running.

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -3,21 +3,25 @@
 echo "Target:     " $TARGET
 echo "OS:         " $TRAVIS_OS_NAME
 echo "Build tool: " $BUILDTOOL
-echo "Compiler:   " $GXX
+echo "Compiler:   " $CXX
+
+set -x
+
+export DEBIAN_FRONTEND=noninteractive
 
 if [ "$TARGET" = "build" ]; then
   if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    sudo -E apt-get -yq update &>> ~/apt-get-update.log
+    sudo apt-get -qq update
     
     if [ "$BUILDTOOL" = "autoconf" ]; then
-      sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install \
+      sudo apt-get install -y \
         autoconf 
     elif [ "$BUILDTOOL" = "cmake" ]; then
-      sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install \
+      sudo apt-get install -y \
         cmake
     fi
     
-    sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install \
+    sudo apt-get install -y \
       pkg-config \
       libxml2-dev \
       libprotobuf-dev protobuf-compiler \
@@ -25,7 +29,7 @@ if [ "$TARGET" = "build" ]; then
       libcairo2-dev libpangocairo-1.0-0 libpango1.0-dev \
       qt5-default qtdeclarative5-dev libqt5svg5-dev qtlocation5-dev \
       freeglut3 freeglut3-dev \
-      libmarisa-dev
+      libmarisa-dev      
   elif  [ "$TRAVIS_OS_NAME" = "osx" ]; then
     brew update
     
