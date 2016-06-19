fcbcbc3553b61980ca30b51ec5a85ad02c01964a
tteuling <tteuling@users.sourceforge.net>
2016-06-19 21:12:50 +0200

    Moved package installation for travis to script, added empty slot for building the web site.

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-if [ "$BUILDTOOL" = "autoconf" ]; then
-  make full
-  (cd libosmscout/tests && make check)
-else
-  mkdir build
-  cd build
-  cmake ..
-  make
+if [ "$TARGET" = "build" ]; then
+  if [ "$BUILDTOOL" = "autoconf" ]; then
+    make full
+    (cd libosmscout/tests && make check)
+  elif [ "$BUILDTOOL" = "cmake" ]; then
+    mkdir build
+    cd build
+    cmake ..
+    make
+  fi
+elif [ "$TARGET" = "website" ]; then
+  echo "Building website..."
 fi
+  
 
