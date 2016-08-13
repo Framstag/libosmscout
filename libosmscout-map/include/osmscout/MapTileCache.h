805498248a96cce163ac32bf84280ea526c6ebca
tteuling <tteuling@users.sourceforge.net>
2016-08-13 11:30:27 +0200

    Made MapTileCache a template.

--- libosmscout-map/include/osmscout/MapTileCache.h
+++ libosmscout-map/include/osmscout/MapTileCache.h
@@ -34,16 +34,193 @@
 
 namespace osmscout {
 
+  template<class T>
+  class MapTileCache
+  {
+  public:
+    typedef typename std::shared_ptr<T> TRef;
+
+  private:
+    /**
+     * Internally used cache entry
+     */
+    struct OSMSCOUT_MAP_API CacheEntry
+    {
+      TileId id;
+      TRef   tile;
+
+      CacheEntry(const TileId& id,
+                 const TRef& tile)
+              : id(id),
+                tile(tile)
+      {
+        // no code
+      }
+    };
+
+    //! A list of cached tiles
+    typedef typename std::list<CacheEntry>     Cache;
+
+    //! References to a tile in above list
+    typedef typename Cache::iterator           CacheRef;
+
+    //! An index from TileIds to cache entries
+    typedef typename std::map<TileId,CacheRef> CacheIndex;
+
+  private:
+    size_t             cacheSize;
+
+    mutable CacheIndex tileIndex;
+    mutable Cache      tileCache;
+
+  public:
+    MapTileCache(size_t cacheSize);
+
+    void SetSize(size_t cacheSize);
+
+    void CleanupCache();
+
+    MapTileCache::TRef GetCachedTile(const TileId& id) const;
+    MapTileCache::TRef GetTile(const TileId& id) const;
+
+    void GetTilesForBoundingBox(const Magnification& magnification,
+                                const GeoBox& boundingBox,
+                                std::list<MapTileCache::TRef>& tiles) const;
+  };
+
+  /**
+   * Create a new tile cache with the given cache size
+   */
+  template <class T>
+  MapTileCache<T>::MapTileCache(size_t cacheSize)
+    : cacheSize(cacheSize)
+  {
+    // no code
+  }
+
+  /**
+   * Change the size of the cache. Cache will be cleaned immediately.
+   */
+  template <class T>
+  void MapTileCache<T>::SetSize(size_t cacheSize)
+  {
+    bool cleanupCache=cacheSize<this->cacheSize;
+
+    this->cacheSize=cacheSize;
+
+    if (cleanupCache) {
+      CleanupCache();
+    }
+  }
+
+  /**
+   * Cleanup the cache. Free least recently used tiles until the given maximum cache
+   * size is reached again.
+   */
+  template <class T>
+  void MapTileCache<T>::CleanupCache()
+  {
+    if (tileCache.size()>cacheSize) {
+      auto currentEntry=tileCache.rbegin();
+
+      while (currentEntry!=tileCache.rend() &&
+             tileCache.size()>cacheSize) {
+        if (currentEntry->tile.use_count()==1) {
+          tileIndex.erase(currentEntry->id);
+
+          ++currentEntry;
+          currentEntry=std::reverse_iterator<MapTileCache<T>::CacheRef>(tileCache.erase(currentEntry.base()));
+        }
+        else {
+          ++currentEntry;
+        }
+      }
+    }
+  }
+
+  /**
+   * Return the cache tiles with the given id. If the tiles is not cache,
+   * an empty reference will be returned.
+   */
+  template <class T>
+  typename MapTileCache<T>::TRef MapTileCache<T>::GetCachedTile(const TileId& id) const
+  {
+    typename std::map<TileId,MapTileCache<T>::CacheRef>::iterator existingEntry=tileIndex.find(id);
+
+    if (existingEntry!=tileIndex.end()) {
+      tileCache.splice(tileCache.begin(),tileCache,existingEntry->second);
+      existingEntry->second=tileCache.begin();
+
+      return existingEntry->second->tile;//.lock();
+    }
+
+    return NULL;
+  }
+
+  /**
+   * Return the tile with the given id. If the tile is not currently cached
+   * return an empty and unassigned tile and move it to the front of the cache.
+   */
+  template <class T>
+  typename MapTileCache<T>::TRef MapTileCache<T>::GetTile(const TileId& id) const
+  {
+    typename std::map<TileId,MapTileCache<T>::CacheRef>::iterator existingEntry=tileIndex.find(id);
+
+    if (existingEntry==tileIndex.end()) {
+      T tile(new T(id));
+
+      // Updating cache
+      typename MapTileCache<T>::CacheEntry cacheEntry(id,tile);
+
+      tileCache.push_front(cacheEntry);
+      tileIndex[id]=tileCache.begin();
+
+      return tile;
+    }
+    else {
+      tileCache.splice(tileCache.begin(),tileCache,existingEntry->second);
+      existingEntry->second=tileCache.begin();
+
+      return existingEntry->second->tile;
+    }
+  }
+
+  /**
+   * Return all tile necessary for covering the given boundingbox using the given magnification.
+   */
+  template <class T>
+  void MapTileCache<T>::GetTilesForBoundingBox(const Magnification& magnification,
+                                              const GeoBox& boundingBox,
+                                              std::list<typename MapTileCache<T>::TRef>& tiles) const
+  {
+    tiles.clear();
+
+    //log.Debug() << "Creating tile data for level " << level << " and bounding box " << boundingBox.GetDisplayText();
+
+    uint32_t level=magnification.GetLevel();
+
+    uint32_t cx1=(uint32_t)floor((boundingBox.GetMinLon()+180.0)/cellDimension[level].width);
+    uint32_t cy1=(uint32_t)floor((boundingBox.GetMinLat()+90.0)/cellDimension[level].height);
+
+    uint32_t cx2=(uint32_t)floor((boundingBox.GetMaxLon()+180.0)/cellDimension[level].width);
+    uint32_t cy2=(uint32_t)floor((boundingBox.GetMaxLat()+90.0)/cellDimension[level].height);
+
+    //std::cout << "Tile bounding box: " << cx1 << "," << cy1 << " - "  << cx2 << "," << cy2 << std::endl;
+
+    for (size_t y=cy1; y<=cy2; y++) {
+      for (size_t x=cx1; x<=cx2; x++) {
+        tiles.push_back(GetTile(TileId(magnification,x,y)));
+      }
+    }
+  }
+
   class OSMSCOUT_MAP_API MapTile
   {
   private:
-    TileId       id;                //!< Id of the tile
-
-  private:
-    MapTile(const TileId& id);
+    TileId  id;                //!< Id of the tile
 
   public:
-    friend class MapTileCache;
+    MapTile(const TileId& id);
 
     ~MapTile();
 
@@ -65,58 +242,6 @@ namespace osmscout {
   };
 
   typedef std::shared_ptr<MapTile> MapTileRef;
-
-  class OSMSCOUT_MAP_API MapTileCache
-  {
-  private:
-    /**
-     * Internally used cache entry
-     */
-    struct OSMSCOUT_MAP_API CacheEntry
-    {
-      TileId     id;
-      MapTileRef tile;
-
-      CacheEntry(const TileId& id,
-                 const MapTileRef& tile)
-              : id(id),
-                tile(tile)
-      {
-        // no code
-      }
-    };
-
-    //! A list of cached tiles
-    typedef std::list<CacheEntry>     Cache;
-
-    //! References to a tile in above list
-    typedef Cache::iterator           CacheRef;
-
-    //! An index from TileIds to cache entries
-    typedef std::map<TileId,CacheRef> CacheIndex;
-
-  private:
-    size_t             cacheSize;
-
-    mutable CacheIndex tileIndex;
-    mutable Cache      tileCache;
-
-  public:
-    MapTileCache(size_t cacheSize);
-
-    void SetSize(size_t cacheSize);
-
-    void CleanupCache();
-
-    MapTileRef GetCachedTile(const TileId& id) const;
-    MapTileRef GetTile(const TileId& id) const;
-
-    void GetTilesForBoundingBox(const Magnification& magnification,
-                                const GeoBox& boundingBox,
-                                std::list<MapTileRef>& tiles) const;
-  };
-
-  typedef std::shared_ptr<MapTileCache> MapTileCacheRef;
 }
 
 #endif
