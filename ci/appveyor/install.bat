f7c9d02527719ec69a9600a7f491476a3ba37f99
tteuling <tteuling@users.sourceforge.net>
2016-08-28 13:27:42 +0200

    Even more debugging...

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -10,29 +10,39 @@ echo MSYS2 system: %MSYSTEM%
 echo Configuration: %CONFIGURATION%
 echo Bits: %BIT%
 
+@echo on
+
+echo Start updating build dependencies...
+
 IF %COMPILER%==msys2 (
-  @echo on
+  echo MSYS build...
+  echo Extending path to MSYS...
   SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
+  echo "Updating dependencies...
   bash -lc "pacman -S --needed --noconfirm pacman-mirrors"
   bash -lc "pacman -S --needed --noconfirm git"
   
   IF %BUILDTOOL%==autoconf (
+  echo Installing autoconf tools...
   bash -lc "pacman -S --needed --noconfirm git autoconf automake make"
   ) ELSE (
+  echo Installing cmake tools...
   bash -lc "pacman -S --needed --noconfirm git make mingw-w64-x86_64-cmake mingw-w64-x86_64-extra-cmake-modules"
   )
   
+  echo Installing build and compile time dependencies...
   bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-toolchain mingw-w64-x86_64-libtool mingw-w64-x86_64-protobuf mingw-w64-x86_64-libxml2 mingw-w64-x86_64-cairo mingw-w64-x86_64-pango mingw-w64-x86_64-qt5"
 )
 
 IF %COMPILER%==msvc2015 (
+  echo MSVC2015 build...
   echo Installing 7zip command line tools...
   
   cinst wget -x86
   cinst 7zip.commandline -x86
   
   echo Adding '%appveyor_build_folder%\7ZipCLI\tools' to path
-  set PATH=%appveyor_build_folder%\7ZipCLI\tools;%PATH%
+  set "PATH=%appveyor_build_folder%\7ZipCLI\tools;%PATH%"
 
   IF %PLATFORM%==x64 (
     wget -q ftp://ftp.zlatkovic.com/libxml/64bit/zlib-1.2.8-win32-x86_64.7z -O zlib-1.2.8-win32-x86_64.7z
