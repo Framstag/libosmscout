393eb43315fd57f07abc55fd7d7d5dd468b93dcd
Lukáš Karas <lukas.karas@centrum.cz>
2016-10-23 19:49:12 +0200

    add script for test build in all docker environments, fix relative path to dockerfiles

---
+++ ci/docker/buildAll.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd `dirname $0`
+set -xe
+
+./archlinux_clang_autoconf/build.sh
+./archlinux_gcc_autoconf/build.sh
+./archlinux_gcc_cmake/build.sh
+./debian_jessie_gcc_autoconf/build.sh
+./debian_jessie_gcc_cmake/build.sh
+./ubuntu_14.04_gcc_autoconf/build.sh
+./ubuntu_14.04_gcc_cmake/build.sh
+./ubuntu_15.10_gcc_autoconf/build.sh
+./ubuntu_15.10_gcc_cmake/build.sh
+./ubuntu_16.04_gcc_autoconf/build.sh
+./ubuntu_16.04_gcc_cmake/build.sh
+./ubuntu_16.10_gcc_autoconf/build.sh
+./ubuntu_16.10_gcc_cmake/build.sh
