5daa780cf0c839d69391d48494bac02c17b11995
Transporter <Ogre.Transporter@gmail.com>
2016-12-01 04:22:33 -0800

    Part three of Windows file size bugfix

--- libosmscout-import/src/osmscout/import/PreprocessPBF.cpp
+++ libosmscout-import/src/osmscout/import/PreprocessPBF.cpp
@@ -50,7 +50,16 @@ namespace osmscout {
   bool PreprocessPBF::GetPos(FILE* file,
                              FileOffset& pos) const
   {
-#if defined(HAVE_FSEEKO)
+#if defined(__WIN32__) || defined(WIN32)
+	  const __int64 filepos = _ftelli64(file);
+
+	  if (filepos == -1) {
+		  return false;
+	  }
+	  else {
+		  pos = (FileOffset)filepos;
+	  }
+#elif defined(HAVE_FSEEKO)
     off_t filepos=ftello(file);
 
     if (filepos==-1) {
