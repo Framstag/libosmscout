7bca1ab658e6edae031be6291b891d7d7fc0e606
tteuling <tteuling@users.sourceforge.net>
2017-02-09 21:31:43 +0100

    Next try 32 bit msys build.

--- ci/appveyor/build.bat
+++ ci/appveyor/build.bat
@@ -8,7 +8,6 @@ echo Platform: %PLATFORM%
 echo MSYS2 directory: %MSYS2_DIR%
 echo MSYS2 system: %MSYSTEM%
 echo Configuration: %CONFIGURATION%
-echo Bits: %BIT%
 echo Buildtool: %BUILDTOOL%
 echo Target: %TARGET%
 
@@ -28,33 +27,33 @@ IF %COMPILER%==msys2 (
     ) ELSE (
         bash -lc "cd ${APPVEYOR_BUILD_FOLDER} && . setupMSYS2.sh && exec 0</dev/null && mkdir build && cd build && CXX=g++ CC=gcc cmake -G 'MSYS Makefiles' .. && cmake -LAH .. && make -j2"
     )
-  )  
+  )
 )
 
 IF %COMPILER%==msvc2015 (
   @echo on
   echo "Compiling libosmscout using Visual Studio 2015..."
-  
+
   IF %BUILDTOOL%==msbuild (
     echo "Using build tool 'msbuild'..."
 
-    copy windows\libosmscout\include\osmscout\CoreFeatures.h libosmscout\include\osmscout\CoreFeatures.h 
-    copy windows\libosmscout\include\osmscout\private\Config.h  libosmscout\include\osmscout\private\Config.h 
+    copy windows\libosmscout\include\osmscout\CoreFeatures.h libosmscout\include\osmscout\CoreFeatures.h
+    copy windows\libosmscout\include\osmscout\private\Config.h  libosmscout\include\osmscout\private\Config.h
     copy windows\libosmscout\msvc2015_libosmscout.vcxproj libosmscout\libosmscout.vcxproj
 
-    copy windows\libosmscout-import\include\osmscout\ImportFeatures.h libosmscout-import\include\osmscout\ImportFeatures.h 
-    copy windows\libosmscout-import\include\osmscout\private\Config.h libosmscout-import\include\osmscout\private\Config.h 
-    copy windows\libosmscout-import\msvc2015_libosmscout-import.vcxproj libosmscout-import\libosmscout-import.vcxproj 
+    copy windows\libosmscout-import\include\osmscout\ImportFeatures.h libosmscout-import\include\osmscout\ImportFeatures.h
+    copy windows\libosmscout-import\include\osmscout\private\Config.h libosmscout-import\include\osmscout\private\Config.h
+    copy windows\libosmscout-import\msvc2015_libosmscout-import.vcxproj libosmscout-import\libosmscout-import.vcxproj
 
-    copy windows\libosmscout-map\include\osmscout\MapFeatures.h libosmscout-map\include\osmscout\MapFeatures.h 
-    copy windows\libosmscout-map\include\osmscout\private\Config.h  libosmscout-map\include\osmscout\private\Config.h 
-    copy windows\libosmscout-map\msvc2015_libosmscout-map.vcxproj libosmscout-map\libosmscout-map.vcxproj 
+    copy windows\libosmscout-map\include\osmscout\MapFeatures.h libosmscout-map\include\osmscout\MapFeatures.h
+    copy windows\libosmscout-map\include\osmscout\private\Config.h  libosmscout-map\include\osmscout\private\Config.h
+    copy windows\libosmscout-map\msvc2015_libosmscout-map.vcxproj libosmscout-map\libosmscout-map.vcxproj
 
     copy windows\Import\msvc2015_Import.vcxproj Import\Import.vcxproj
 
-    copy windows\msvc2015_libosmscout.sln libosmscout.sln   
+    copy windows\msvc2015_libosmscout.sln libosmscout.sln
 
-    "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% libosmscout.sln   
+    "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% libosmscout.sln
   ) ELSE (
     echo "Using build tool 'cmake'..."
 
