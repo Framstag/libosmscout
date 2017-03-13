2b31bb6b6b620059cf0bfe578e7a28cb9988c978
tteuling <tteuling@users.sourceforge.net>
2017-03-13 20:19:52 +0100

    Dropped the manually managed Windows project files. Removed the msbuild based Appveyor build that uses these files, too.

--- ci/appveyor/build.bat
+++ ci/appveyor/build.bat
@@ -33,33 +33,10 @@ IF %COMPILER%==msys2 (
 IF %COMPILER%==msvc2015 (
   @echo on
   echo "Compiling libosmscout using Visual Studio 2015..."
+  echo "Using build tool 'cmake'..."
 
-  IF %BUILDTOOL%==msbuild (
-    echo "Using build tool 'msbuild'..."
-
-    copy windows\libosmscout\include\osmscout\CoreFeatures.h libosmscout\include\osmscout\CoreFeatures.h
-    copy windows\libosmscout\include\osmscout\private\Config.h  libosmscout\include\osmscout\private\Config.h
-    copy windows\libosmscout\msvc2015_libosmscout.vcxproj libosmscout\libosmscout.vcxproj
-
-    copy windows\libosmscout-import\include\osmscout\ImportFeatures.h libosmscout-import\include\osmscout\ImportFeatures.h
-    copy windows\libosmscout-import\include\osmscout\private\Config.h libosmscout-import\include\osmscout\private\Config.h
-    copy windows\libosmscout-import\msvc2015_libosmscout-import.vcxproj libosmscout-import\libosmscout-import.vcxproj
-
-    copy windows\libosmscout-map\include\osmscout\MapFeatures.h libosmscout-map\include\osmscout\MapFeatures.h
-    copy windows\libosmscout-map\include\osmscout\private\Config.h  libosmscout-map\include\osmscout\private\Config.h
-    copy windows\libosmscout-map\msvc2015_libosmscout-map.vcxproj libosmscout-map\libosmscout-map.vcxproj
-
-    copy windows\Import\msvc2015_Import.vcxproj Import\Import.vcxproj
-
-    copy windows\msvc2015_libosmscout.sln libosmscout.sln
-
-    "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% libosmscout.sln
-  ) ELSE (
-    echo "Using build tool 'cmake'..."
-
-    mkdir build
-    cd build
-    cmake -G "Visual Studio 14 2015 Win64" ..
-    cmake --build .
-  )
+  mkdir build
+  cd build
+  cmake -G "Visual Studio 14 2015 Win64" ..
+  cmake --build .
 )
--- windows/libosmscout-map/include/osmscout/MapFeatures.h
+++
@@ -1,8 +0,0 @@
-#ifndef OSMSCOUT_MAP_FEATURES
-#define OSMSCOUT_MAP_FEATURES
-
-/* Illumination uses special gcc compiler features to explicitely export symbols */
-/* #undef OSMSCOUT_MAP_EXPORT_SYMBOLS */
-
-#endif
-
