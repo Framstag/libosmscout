efd2be8143b039f1d1a3722485f5fc1be7499ac1
tteuling <tteuling@users.sourceforge.net>
2017-04-04 20:23:58 +0200

    Increase file format version.

--- libosmscout/include/osmscout/TypeConfig.h
+++ libosmscout/include/osmscout/TypeConfig.h
@@ -1099,7 +1099,7 @@ namespace osmscout {
 
   typedef std::shared_ptr<FeatureValueBuffer> FeatureValueBufferRef;
 
-  static const uint32_t FILE_FORMAT_VERSION=11;
+  static const uint32_t FILE_FORMAT_VERSION=12;
 
   /**
    * \ingroup type
