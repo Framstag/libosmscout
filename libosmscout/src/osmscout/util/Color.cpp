df33265f2e41be493ad5da42658512f0f49d99c2
tteuling <tteuling@users.sourceforge.net>
2017-05-06 16:40:21 +0200

    Changed parsing/printing methods for Color.

--- libosmscout/src/osmscout/util/Color.cpp
+++ libosmscout/src/osmscout/util/Color.cpp
@@ -19,6 +19,8 @@
 
 #include <osmscout/util/Color.h>
 
+#include <osmscout/system/Math.h>
+
 namespace osmscout {
 
   const Color Color::BLACK(0.0,0.0,0.0);
@@ -38,31 +40,44 @@ namespace osmscout {
     }
   }
 
+  static size_t GetHexValue(char digit)
+  {
+    if (digit>='0' && digit<='9') {
+      return digit-'0';
+    }
+    else if (digit>='a' && digit<='f') {
+      return 10+(digit-'a');
+    }
+
+    assert(false);
+    return 0;
+  }
+
   std::string Color::ToHexString() const
   {
     if (a!=1.0) {
-      std::string hexString("        ");
+      std::string hexString("#        ");
 
-      hexString[0]=GetHexChar(((size_t)(r*256.0))/16);
-      hexString[1]=GetHexChar(((size_t)(r*256.0))%16);
-      hexString[2]=GetHexChar(((size_t)(g*256.0))/16);
-      hexString[3]=GetHexChar(((size_t)(g*256.0))%16);
-      hexString[4]=GetHexChar(((size_t)(b*256.0))/16);
-      hexString[5]=GetHexChar(((size_t)(b*256.0))%16);
-      hexString[6]=GetHexChar(((size_t)(a*256.0))/16);
-      hexString[7]=GetHexChar(((size_t)(a*256.0))%16);
+      hexString[1]=GetHexChar((size_t)(r*255.0/16.0));
+      hexString[2]=GetHexChar((size_t)fmod(r*255.0,16));
+      hexString[3]=GetHexChar((size_t)(g*255.0/16.0));
+      hexString[4]=GetHexChar((size_t)fmod(g*255.0,16));
+      hexString[5]=GetHexChar((size_t)(b*255.0/16.0));
+      hexString[5]=GetHexChar((size_t)fmod(b*255.0,16));
+      hexString[6]=GetHexChar((size_t)(a*255.0/16.0));
+      hexString[7]=GetHexChar((size_t)fmod(a*255.0,16));
 
       return hexString;
     }
     else {
-      std::string hexString("      ");
+      std::string hexString("#      ");
 
-      hexString[0]=GetHexChar(((size_t)(r*256.0))/16);
-      hexString[1]=GetHexChar(((size_t)(r*256.0))%16);
-      hexString[2]=GetHexChar(((size_t)(g*256.0))/16);
-      hexString[3]=GetHexChar(((size_t)(g*256.0))%16);
-      hexString[4]=GetHexChar(((size_t)(b*256.0))/16);
-      hexString[5]=GetHexChar(((size_t)(b*256.0))%16);
+      hexString[1]=GetHexChar((size_t)(r*255.0/16.0));
+      hexString[2]=GetHexChar((size_t)fmod(r*255.0,16));
+      hexString[3]=GetHexChar((size_t)(g*255.0/16.0));
+      hexString[4]=GetHexChar((size_t)fmod(g*255.0,16));
+      hexString[5]=GetHexChar((size_t)(b*255.0/16.0));
+      hexString[6]=GetHexChar((size_t)fmod(b*255.0,16));
 
       return hexString;
     }
@@ -83,6 +98,35 @@ namespace osmscout {
     }
 
     return a<other.a;
+  }
+
+  /**
+   * Convert the given color string to a color value
+   *
+   * The string must either be of the format
+   * - #HHHHHH
+   * - #HHHHHHHH
+   *
+   * where '#' is the symbol itself and 'H' represents a hexadecimal value
+   *
+   * @param hexString
+   * @return
+   */
+  Color Color::FromHexString(const std::string& hexString)
+  {
+    double r=(16*GetHexValue(hexString[1])+GetHexValue(hexString[2]))/255.0;
+    double g=(16*GetHexValue(hexString[3])+GetHexValue(hexString[4]))/255.0;
+    double b=(16*GetHexValue(hexString[5])+GetHexValue(hexString[6]))/255.0;
+    double a;
+
+    if (hexString.length()==9) {
+      a=(16*GetHexValue(hexString[7])+GetHexValue(hexString[8]))/255.0;
+    }
+    else {
+      a=1.0;
+    }
+
+    return Color(r,g,b,a);
   }
 }
 
