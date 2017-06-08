952bbe8cb25b9dabc0c42a99f56609f0df643d8d
fannymonori <fannymonori@gmail.com>
2017-06-08 13:47:42 +0200

    make compatible with older glfw3

--- OSMScoutOpenGL/src/OSMScoutOpenGL.cpp
+++ OSMScoutOpenGL/src/OSMScoutOpenGL.cpp
@@ -26,7 +26,7 @@
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, GLFW_TRUE);
+    glfwSetWindowShouldClose(window, true);
 }
 
 bool button_down = false;
