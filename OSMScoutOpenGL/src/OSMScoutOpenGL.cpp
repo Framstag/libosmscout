a2cf89bb7f5efd27915aa5468b622ce3454a9404
Tim Teulings <tim@framstag.com>
2017-07-10 21:11:21 +0200

    Fixed broken merge.

--- OSMScoutOpenGL/src/OSMScoutOpenGL.cpp
+++ OSMScoutOpenGL/src/OSMScoutOpenGL.cpp
@@ -94,7 +94,7 @@ void ErrorCallback(int, const char *err_str) {
 }
 
 static void key_callback(GLFWwindow *window, int key, int /*scancode*/, int action, int /*mods*/) {
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
     glfwSetWindowShouldClose(window, true);
   }
   if(key == GLFW_KEY_LEFT){
