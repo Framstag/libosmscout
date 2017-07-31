e7c7d2e7ebab6f029ac64fb1fa3242bd5284eada
Tim Teulings <tim@framstag.com>
2017-07-31 21:56:49 +0200

    Try to fix windows build.

--- libosmscout/src/osmscout/util/String.cpp
+++ libosmscout/src/osmscout/util/String.cpp
@@ -551,12 +551,13 @@ namespace osmscout {
   {
 #if defined(_MSC_VER) && _MSC_VER >= 1900
     // See https://stackoverflow.com/questions/30765256/linker-error-using-vs-2015-rc-cant-find-symbol-related-to-stdcodecvt
-    std::wstring_convert<std::codecvt_utf8<__int32>, __int32> conv;
+    std::wstring_convert<std::codecvt_utf8<int32_t>, int32_t> conv;
+	return reinterpret_cast<const char32_t*>(conv.from_bytes(text).c_str());
 #else
     std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
+	return conv.from_bytes(text);
 #endif
 
-    return conv.from_bytes(text);
   }
 #else
   #error "Missing implementation for std::wstring UTF8StringToU32String(const std::string& text)"
