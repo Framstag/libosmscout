d2751a9b096f84237ba4b618ceb4ba885ab7b79b
Lukáš Karas <lukas.karas@centrum.cz>
2017-09-23 20:53:18 +0200

    use lowest possible OpenGL version hint
    If we are using higher version hint, demo is not possible launch
    on older hardware (Intel Ivybridge Mobile). Lowest OpenGL version that
    is supporting profiles is 3.2.

--- OSMScoutOpenGL/src/OSMScoutOpenGL.cpp
+++ OSMScoutOpenGL/src/OSMScoutOpenGL.cpp
@@ -300,8 +300,8 @@ int main(int argc, char *argv[]) {
   glfwSetErrorCallback(ErrorCallback);
   if (!glfwInit())
     return -1;
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
