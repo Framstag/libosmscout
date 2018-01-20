12b38342972df9b41eb95f69ef88a32ee09c622e
tteuling <tteuling@users.sourceforge.net>
2018-01-20 21:45:30 +0100

    Issue #542: Reactivated tests for iOS, but do not run them. This will not work, since iOS generates binaries for an architecture, which is not the architecture of the build system ( at least I assume so).

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -44,11 +44,11 @@ if [ "$TARGET" = "build" ]; then
 
     make
 
-#    if  [ "$TRAVIS_OS_NAME" = "osx" ] && [ "$PLATFORM" = "ios" ] ; then
-#        echo "Skip test execution for iOS platform"
-#    else
+    if  [ "$TRAVIS_OS_NAME" = "osx" ] && [ "$PLATFORM" = "ios" ] ; then
+        echo "Skip test execution for iOS platform"
+    else
         make test
-#    fi
+    fi
   fi
 elif [ "$TARGET" = "importer" ]; then
     packaging/import/linux/build_import.sh
