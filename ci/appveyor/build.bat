0fd8c62edb245a456406eff432c8e586e6faa2b3
Lukáš Karas <lukas.karas@centrum.cz>
2018-02-02 20:30:44 +0100

    add symlink to workaround meson bug for windows build

--- ci/appveyor/build.bat
+++ ci/appveyor/build.bat
@@ -18,7 +18,7 @@ IF %COMPILER%==msys2 (
 
   IF %BUILDTOOL%==meson (
     echo Using build tool 'meson'...
-    bash -lc "cd ${APPVEYOR_BUILD_FOLDER} && . setupMSYS2.sh && exec 0</dev/null && meson debug && cd debug && ninja"
+    bash -lc "cd ${APPVEYOR_BUILD_FOLDER} && . setupMSYS2.sh && exec 0</dev/null && meson debug && cd debug && ln -s . ../OSMScout2/OSMScout2 && ln -s . ../StyleEditor/StyleEditor && ninja"
     echo Finished mason build
   )
 
@@ -56,10 +56,6 @@ IF %COMPILER%==msvc2015 (
     mkdir debug
     meson debug --backend vs2015
     cd debug
-
-    # workaround for meson 0.44 issue https://github.com/mesonbuild/meson/issues/2763
-    mklink ../OSMScout2/OSMScout2 .
-    mklink ../StyleEditor/StyleEditor .
 
     msbuild.exe libosmscout.sln /t:build /p:Configuration=debugoptimized /p:Platform="x64"
     echo Finished meson build
