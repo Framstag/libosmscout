1573de525ece84a12f6402bf09047fe2f1978c79
Simon Chopin <simon.chopin@scriptandgo.com>
2018-03-21 15:45:16 +0100

    Use the helper class to draw the contour
    The previous version tended to give a messy output.
    Sadly, the indentation of the files was fucked up and I ordered VS to do
    something about it, hence the huge diff.

--- libosmscout-map-directx/include/osmscout/MapPainterDirectX.h
+++ libosmscout-map-directx/include/osmscout/MapPainterDirectX.h
@@ -36,130 +36,126 @@
 
 namespace osmscout {
 
-	class OSMSCOUT_MAP_DIRECTX_API MapPainterDirectX : public MapPainter
-	{
-	private:
-		typedef std::unordered_map<uint32_t, IDWriteTextFormat*> FontMap;
-		FontMap m_Fonts;
-		typedef std::unordered_map<uint32_t, ID2D1SolidColorBrush*> BrushMap;
-		BrushMap m_Brushs;
-		typedef std::unordered_map<uint64_t, ID2D1PathGeometry*> GeometryMap;
-		GeometryMap m_Geometries;
-		GeometryMap m_Polygons;
-		typedef std::unordered_map<uint64_t, ID2D1Bitmap*> BitmapMap;
-		BitmapMap m_Bitmaps;
-		typedef std::unordered_map<uint64_t, ID2D1StrokeStyle*> StrokeStyleMap;
-		StrokeStyleMap m_StrokeStyles;
-		ID2D1StrokeStyle* m_dashLessStrokeStyle;
+  class OSMSCOUT_MAP_DIRECTX_API MapPainterDirectX : public MapPainter
+  {
+  private:
+    typedef std::unordered_map<uint32_t, IDWriteTextFormat*> FontMap;
+    FontMap m_Fonts;
+    typedef std::unordered_map<uint32_t, ID2D1SolidColorBrush*> BrushMap;
+    BrushMap m_Brushs;
+    typedef std::unordered_map<uint64_t, ID2D1PathGeometry*> GeometryMap;
+    GeometryMap m_Geometries;
+    GeometryMap m_Polygons;
+    typedef std::unordered_map<uint64_t, ID2D1Bitmap*> BitmapMap;
+    BitmapMap m_Bitmaps;
+    typedef std::unordered_map<uint64_t, ID2D1StrokeStyle*> StrokeStyleMap;
+    StrokeStyleMap m_StrokeStyles;
+    ID2D1StrokeStyle* m_dashLessStrokeStyle;
 
-		ID2D1Factory* m_pDirect2dFactory;
-		IDWriteFactory* m_pWriteFactory;
-		ID2D1RenderTarget* m_pRenderTarget;
-		IWICImagingFactory* m_pImagingFactory;
-		IDWriteRenderingParams* m_pRenderingParams;
-		PathTextRenderer* m_pPathTextRenderer;
-		FLOAT dpiX, dpiY;
-		std::map<double, double> fontHeightMap;
-		TypeConfigRef typeConfig;
+    ID2D1Factory* m_pDirect2dFactory;
+    IDWriteFactory* m_pWriteFactory;
+    ID2D1RenderTarget* m_pRenderTarget;
+    IWICImagingFactory* m_pImagingFactory;
+    IDWriteRenderingParams* m_pRenderingParams;
+    PathTextRenderer* m_pPathTextRenderer;
+    FLOAT dpiX, dpiY;
+    std::map<double, double> fontHeightMap;
+    TypeConfigRef typeConfig;
 
-	private:
-		D2D1_COLOR_F GetColorValue(const Color& color);
-		ID2D1SolidColorBrush* GetColorBrush(const Color& color);
-		ID2D1SolidColorBrush* GetColorBrush(D2D1_COLOR_F& color);
-		ID2D1StrokeStyle* GetStrokeStyle(const std::vector<double>& dash);
-		void _DrawText(const Projection& projection, const MapParameter& parameter, double x, double y, double fontSize, const Color& color, std::string text);
-		bool LoadBitmapFromFile(PCWSTR uri, ID2D1Bitmap **ppBitmap);
-		IDWriteTextFormat* GetFont(const Projection& projection, const MapParameter& parameter, double fontSize);
+  private:
+    D2D1_COLOR_F GetColorValue(const Color& color);
+    ID2D1SolidColorBrush* GetColorBrush(const Color& color);
+    ID2D1SolidColorBrush* GetColorBrush(D2D1_COLOR_F& color);
+    ID2D1StrokeStyle* GetStrokeStyle(const std::vector<double>& dash);
+    void _DrawText(const Projection& projection, const MapParameter& parameter, double x, double y, double fontSize, const Color& color, std::string text);
+    bool LoadBitmapFromFile(PCWSTR uri, ID2D1Bitmap **ppBitmap);
+    IDWriteTextFormat* GetFont(const Projection& projection, const MapParameter& parameter, double fontSize);
 
-	protected:
-		virtual void AfterPreprocessing(const StyleConfig& styleConfig,
-			const Projection& projection,
-			const MapParameter& parameter,
-			const MapData& data);
+  protected:
+    virtual void AfterPreprocessing(const StyleConfig& styleConfig,
+                                    const Projection& projection,
+                                    const MapParameter& parameter,
+                                    const MapData& data);
 
-		virtual void BeforeDrawing(const StyleConfig& styleConfig,
-			const Projection& projection,
-			const MapParameter& parameter,
-			const MapData& data);
+    virtual void BeforeDrawing(const StyleConfig& styleConfig,
+                               const Projection& projection,
+                               const MapParameter& parameter,
+                               const MapData& data);
 
-		virtual void AfterDrawing(const StyleConfig& styleConfig,
-			const Projection& projection,
-			const MapParameter& parameter,
-			const MapData& data);
+    virtual void AfterDrawing(const StyleConfig& styleConfig,
+                              const Projection& projection,
+                              const MapParameter& parameter,
+                              const MapData& data);
 
-		virtual bool HasIcon(const StyleConfig& styleConfig,
-			const MapParameter& parameter,
-			IconStyle& style);
+    virtual bool HasIcon(const StyleConfig& styleConfig,
+                         const MapParameter& parameter,
+                         IconStyle& style);
 
-		virtual double GetFontHeight(const Projection& projection,
-			const MapParameter& parameter,
-			double fontSize);
+    virtual double GetFontHeight(const Projection& projection,
+                                 const MapParameter& parameter,
+                                 double fontSize);
 
-		TextDimension GetTextDimension(const Projection& projection,
-			const MapParameter& parameter,
-      double objectWidth,
-			double fontSize,
-			const std::string& text);
+    TextDimension GetTextDimension(const Projection& projection,
+                                   const MapParameter& parameter,
+                                   double objectWidth,
+                                   double fontSize,
+                                   const std::string& text);
 
-		/*virtual void GetLabelFrame(const LabelStyle& style,
-			double& horizontal,
-			double& vertical);*/
+    virtual void DrawGround(const Projection& projection,
+                            const MapParameter& parameter,
+                            const FillStyle& style);
 
-		virtual void DrawGround(const Projection& projection,
-			const MapParameter& parameter,
-			const FillStyle& style);
+    virtual void DrawLabel(const Projection& projection,
+                           const MapParameter& parameter,
+                           const LabelData& label);
 
-		virtual void DrawLabel(const Projection& projection,
-			const MapParameter& parameter,
-			const LabelData& label);
+    virtual void DrawIcon(const IconStyle* style,
+                          double x, double y);
 
-		virtual void DrawIcon(const IconStyle* style,
-			double x, double y);
+    virtual void DrawSymbol(const Projection& projection,
+                            const MapParameter& parameter,
+                            const Symbol& symbol,
+                            double x, double y);
 
-		virtual void DrawSymbol(const Projection& projection,
-			const MapParameter& parameter,
-			const Symbol& symbol,
-			double x, double y);
+    virtual void DrawPath(const Projection& projection,
+                          const MapParameter& parameter,
+                          const Color& color,
+                          double width,
+                          const std::vector<double>& dash,
+                          LineStyle::CapStyle startCap,
+                          LineStyle::CapStyle endCap,
+                          size_t transStart, size_t transEnd);
 
-		virtual void DrawPath(const Projection& projection,
-			const MapParameter& parameter,
-			const Color& color,
-			double width,
-			const std::vector<double>& dash,
-			LineStyle::CapStyle startCap,
-			LineStyle::CapStyle endCap,
-			size_t transStart, size_t transEnd);
+    virtual void DrawContourLabel(const Projection& projection,
+                                  const MapParameter& parameter,
+                                  const PathTextStyle& style,
+                                  const std::string& text,
+                                  size_t transStart, size_t transEnd,
+                                  ContourLabelHelper& helper);
 
-		virtual void DrawContourLabel(const Projection& projection,
-			const MapParameter& parameter,
-			const PathTextStyle& style,
-			const std::string& text,
-			size_t transStart, size_t transEnd,
-      ContourLabelHelper& helper);
+    virtual void DrawContourSymbol(const Projection& projection,
+                                   const MapParameter& parameter,
+                                   const Symbol& symbol,
+                                   double space,
+                                   size_t transStart, size_t transEnd);
 
-		virtual void DrawContourSymbol(const Projection& projection,
-			const MapParameter& parameter,
-			const Symbol& symbol,
-			double space,
-			size_t transStart, size_t transEnd);
+    virtual void DrawArea(const Projection& projection,
+                          const MapParameter& parameter,
+                          const AreaData& area);
 
-		virtual void DrawArea(const Projection& projection,
-			const MapParameter& parameter,
-			const AreaData& area);
+  public:
+    MapPainterDirectX(const StyleConfigRef& styleConfig,
+                      ID2D1Factory* pDirect2dFactory,
+                      IDWriteFactory* pWriteFactory);
+    virtual ~MapPainterDirectX();
 
-	public:
-		MapPainterDirectX(const StyleConfigRef& styleConfig,
-			ID2D1Factory* pDirect2dFactory,
-			IDWriteFactory* pWriteFactory);
-		virtual ~MapPainterDirectX();
+    void DiscardDeviceResources();
 
-		void DiscardDeviceResources();
-
-		bool DrawMap(const Projection& projection,
-			const MapParameter& parameter,
-			const MapData& data,
-			ID2D1RenderTarget* renderTarget);
-	};
+    bool DrawMap(const Projection& projection,
+                 const MapParameter& parameter,
+                 const MapData& data,
+                 ID2D1RenderTarget* renderTarget);
+  };
 }
 
 #endif
