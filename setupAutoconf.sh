40dfbd8e99b26dc8d88e2a7a6c3aa95444bb7465
tteuling <tteuling@users.sourceforge.net>
2018-03-28 22:03:47 +0200

    Remove autotools support.

--- setupAutoconf.sh
+++
@@ -1,3 +0,0 @@
-export QT_SELECT=5
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${PWD}/libosmscout:${PWD}/libosmscout-import:${PWD}/libosmscout-map:${PWD}/libosmscout-map-svg:${PWD}/libosmscout-map-qt:${PWD}/libosmscout-client-qt:${PWD}/libosmscout-map-agg:${PWD}/libosmscout-map-cairo:${PWD}/libosmscout-map-opengl
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/libosmscout/src/.libs:${PWD}/libosmscout-import/src/.libs:${PWD}/libosmscout-map/src/.libs:${PWD}/libosmscout-map-svg/src/.libs:${PWD}/libosmscout-map-qt/src/.libs:${PWD}/libosmscout-client-qt/src/.libs:${PWD}/libosmscout-map-agg/src/.libs:${PWD}/libosmscout-map-cairo/src/.libs:${PWD}/libosmscout-map-opengl/src/.libs
