a3fa0979cc7ccb75e9f70a191d2929c498544e8c
Lukáš Karas <lukas.karas@centrum.cz>
2018-04-07 23:34:19 +0200

    initial support for contour labels

--- Tests/src/LabelLayouterQt.cpp
+++ Tests/src/LabelLayouterQt.cpp
@@ -50,47 +50,52 @@ template<class NativeGlyph>
 class Glyph {
 public:
   NativeGlyph glyph;
-  osmscout::Vertex2D center;
-  double angle;
+  osmscout::Vertex2D position;
+  double angle{0}; // radians
 };
 
-template<class NativeGlyph>
+template<class NativeGlyph, class NativeLabel>
 class Label
 {
 public:
-  double                   width;
-  double                   height;
+  NativeLabel             label;
 
-  double                   fontSize; //!< Font size to be used
-  osmscout::LabelStyleRef  style;    //!< Style for drawing
-  std::string              text;     //!< The label text
+  double                  width;
+  double                  height;
+
+  double                  fontSize; //!< Font size to be used
+  osmscout::LabelStyleRef style;    //!< Style for drawing
+  std::string             text;     //!< The label text
 
   std::vector<Glyph<NativeGlyph>> toGlyphs() const;
 };
 
-template<class NativeGlyph>
+template<class NativeGlyph, class NativeLabel>
 class LabelInstance
 {
 public:
-  size_t                   id;       //!< Id of this label, multiple labels with the same id do not intersect with each other
-  size_t                   priority; //!< Priority of the entry
+  size_t                  id;       //!< Id of this label, multiple labels with the same id do not intersect with each other
+  size_t                  priority; //!< Priority of the entry
 
-  double                   x;        //!< Coordinate of the left, top edge of the text
-  double                   y;        //!< Coordinate of the left, top edge of the text
-  double                   alpha;    //!< Alpha value of the label
+  double                  x;        //!< Coordinate of the left, top edge of the text
+  double                  y;        //!< Coordinate of the left, top edge of the text
+  double                  alpha;    //!< Alpha value of the label
 
-  Label<NativeGlyph>       label;
+  Label<NativeGlyph, NativeLabel>
+                          label;
 
 public:
   LabelInstance();
   virtual ~LabelInstance();
 };
 
-template <class NativeGlyph, class TextLayouter>
+template <class NativeGlyph, class NativeLabel, class TextLayouter>
 class OSMSCOUT_MAP_API LabelLayouter
 {
 public:
   using ContourLabel = std::vector<Glyph<NativeGlyph>>;
+  using LabelType = Label<NativeGlyph, NativeLabel>;
+  using LabelInstanceType = LabelInstance<NativeGlyph, NativeLabel>;
 
 public:
   LabelLayouter(TextLayouter *textLayouter):
@@ -100,32 +105,66 @@ public:
   void registerContourLabel(std::vector<QPointF> way,
                             std::string string)
   {
+    // TODO: parameters
+    int fontHeight=24;
+    int textOffset=6;
 
+    // TODO: cache simplified path for way id
+    osmscout::SimplifiedPath p;
+    for (auto const &point:way){
+      p.AddPoint(point.x(), point.y());
+    }
+
+    // TODO: cache label for string and font parameters
+    LabelType label = textLayouter->layout(string, fontHeight, /* proposed width */ 5000.0);
+    ContourLabel cLabel = label.toGlyphs();
+
+    double pLength=p.GetLength();
+    double offset=0;
+    while (offset<pLength){
+      ContourLabel copy;
+      for (Glyph<NativeGlyph> glyphCopy:cLabel){
+        double glyphOffset=offset+glyphCopy.position.GetX();
+        QPointF point=p.PointAtLength(glyphOffset);
+        qreal   angle=p.AngleAtLength(glyphOffset);
+        // TODO: take y into account
+        glyphCopy.position=osmscout::Vertex2D(point.x() + textOffset * std::sin(angle),
+                                              point.y() + textOffset * std::cos(angle));
+        glyphCopy.angle=angle;
+
+        copy.push_back(glyphCopy);
+      }
+      contourLabelsVect.push_back(copy);
+      offset+=label.width + 3*fontHeight;
+    }
+    // TODO
   }
 
-  std::vector<LabelInstance<NativeGlyph>> labels() const
+  std::vector<LabelInstanceType> labels() const
   {
-    return std::move(std::vector<LabelInstance<NativeGlyph>>());
+    // TODO
+    return std::move(std::vector<LabelInstanceType>());
   }
 
   std::vector<ContourLabel> contourLabels() const
   {
-    return std::move(std::vector<ContourLabel>());
+    return contourLabelsVect;
   }
 
 private:
   TextLayouter *textLayouter;
+  std::vector<ContourLabel> contourLabelsVect;
 };
 
 
 using QtGlyph = Glyph<QGlyphRun>;
-using QtLabel = Label<QGlyphRun>;
+using QtLabel = Label<QGlyphRun, std::shared_ptr<QTextLayout>>;
 
 class QTextLayouter
 {
 public:
   QPainter *painter;
-  LabelLayouter<QGlyphRun, QTextLayouter> labelLayouter;
+  LabelLayouter<QGlyphRun, std::shared_ptr<QTextLayout>, QTextLayouter> labelLayouter;
 
 public:
   QTextLayouter(QPainter *painter):
@@ -134,11 +173,94 @@ public:
   {
   }
 
+  QtLabel layout(std::string text, int fontSize, double proposedWidth)
+  {
+    QFont         font;
+    qreal width=0;
+    qreal height=0;
+
+    font.setPixelSize(fontSize);
+    font.setStyleStrategy(QFont::PreferAntialias);
+    font.setStyleStrategy(QFont::PreferMatch);
+
+    painter->setFont(font);
+
+    QFontMetrics fontMetrics=QFontMetrics(font, painter->device());
+    qreal leading=fontMetrics.leading();
+
+    QtLabel label;
+
+    label.label = std::make_shared<QTextLayout>(QString::fromUtf8(text.c_str()),font,painter->device());
+    // evaluate layout
+    label.label->beginLayout();
+    while (true) {
+      QTextLine line = label.label->createLine();
+      if (!line.isValid())
+        break;
+
+      line.setLineWidth(proposedWidth);
+
+      height+=leading;
+      line.setPosition(QPointF(0.0,height));
+      width=std::max(width,line.naturalTextWidth());
+      height+=line.height();
+    }
+    label.label->endLayout();
+
+    // Center all lines horizontally, after we know the actual width
+
+    for (int i=0; i<label.label->lineCount(); i++) {
+      QTextLine line = label.label->lineAt(i);
+
+      line.setPosition(QPointF((width-line.naturalTextWidth())/2,line.position().y()));
+    }
+
+    label.width=width;
+    label.height=height;
+    label.fontSize=fontSize;
+    //label.style;
+    label.text=text;
+
+    return label;
+  }
 };
 
 template<> std::vector<QtGlyph> QtLabel::toGlyphs() const
 {
-  return std::vector<QtGlyph>();
+  std::vector<QtGlyph> result;
+  QVector<quint32> indexes(1);
+  QVector<QPointF> positions(1);
+
+  positions[0] = QPointF(0, 0);
+
+  QList<QGlyphRun> glyphs=label->glyphRuns();
+  for (const QGlyphRun &glyphRun: glyphs){
+    for (int g=0; g<glyphRun.glyphIndexes().size(); g++) {
+
+      qint32 index = glyphRun.glyphIndexes().at(g);
+      QPointF pos = glyphRun.positions().at(g);
+      QRectF bbox = glyphRun.boundingRect();
+
+      indexes[0] = index;
+
+      QGlyphRun orphanGlyph;
+      orphanGlyph.setBoundingRect(bbox);
+      orphanGlyph.setFlags(glyphRun.flags());
+      orphanGlyph.setGlyphIndexes(indexes);
+      orphanGlyph.setOverline(glyphRun.overline());
+      orphanGlyph.setPositions(positions);
+      orphanGlyph.setRawFont(glyphRun.rawFont());
+      orphanGlyph.setRightToLeft(glyphRun.isRightToLeft());
+      orphanGlyph.setStrikeOut(glyphRun.strikeOut());
+      orphanGlyph.setUnderline(glyphRun.underline());
+      
+      QtGlyph glyph;
+      glyph.glyph=std::move(orphanGlyph);
+      glyph.position.Set(pos.x(), pos.y());
+      result.push_back(std::move(glyph));
+    }
+  }
+  return result;
 }
 
 class DrawWindow : public QWidget
@@ -213,47 +335,36 @@ void DrawWindow::drawLine(QPainter *painter, const std::vector<QPointF> &way)
 void DrawWindow::drawGlyph(QPainter *painter,
                            const Glyph<QGlyphRun> &glyph) const
 {
+  QPen          pen;
+  pen.setColor(QColor::fromRgbF(0,0,0));
+  painter->setPen(pen);
+
   QTransform tran;
-  osmscout::Vertex2D point=glyph.center;
-  qreal   angle=glyph.angle;
-  qreal penWidth = painter->pen().widthF();
+  const QTransform &originalTran=painter->transform();
+  QPointF point=QPointF(glyph.position.GetX(), glyph.position.GetY());
+  qreal   angle=(glyph.angle * 180) / M_PI;
+  //qreal penWidth = painter->pen().widthF();
 
   // rotation matrix components
+  /*
   qreal sina=sin[lround((360-angle)*10)%sin.size()];
   qreal cosa=sin[lround((360-angle+90)*10)%sin.size()];
 
-  // Rotation
-  qreal newX=(cosa*point.GetX())-(sina*(point.GetY()));
-  qreal newY=(cosa*(point.GetY()))+(sina*point.GetX());
-
-  // Aditional offseting
-  qreal deltaPenX=cosa*penWidth;
-  qreal deltaPenY=sina*penWidth;
-
-  // Getting the delta distance for the translation part of the transformation
-  qreal deltaX=newX-point.GetX();
-  qreal deltaY=newY-point.GetY();
-
   // Applying rotation and translation.
-  tran.setMatrix(cosa,sina,0.0,
-                 -sina,cosa,0.0,
-                 -deltaX+deltaPenX,-deltaY-deltaPenY,1.0);
+  tran.setMatrix(cosa, sina, 0.0,
+                 -sina, cosa, 0.0,
+                 point.x(), point.y(), 1.0);
+
+  */
+  tran.translate(point.x(), point.y());
+  tran.rotateRadians(glyph.angle*-1);
+
+
   painter->setTransform(tran);
 
-  /*
-  QGlyphRun orphanGlyph;
-  //orphanGlyph.setBoundingRect();
-  orphanGlyph.setFlags(glypRun.flags());
-  orphanGlyph.setGlyphIndexes(indexes);
-  orphanGlyph.setOverline(glypRun.overline());
-  orphanGlyph.setPositions(positions);
-  orphanGlyph.setRawFont(glypRun.rawFont());
-  orphanGlyph.setRightToLeft(glypRun.isRightToLeft());
-  orphanGlyph.setStrikeOut(glypRun.strikeOut());
-  orphanGlyph.setUnderline(glypRun.underline());
-  */
+  painter->drawGlyphRun(QPointF(0,0), glyph.glyph);
 
-  painter->drawGlyphRun(QPointF(point.GetX(), point.GetY()), glyph.glyph);
+  painter->setTransform(originalTran);
 
 }
 
@@ -263,9 +374,10 @@ void DrawWindow::paintEvent(QPaintEvent* /* event */)
   painter.fillRect(0,0, width(), height(), QBrush(QColor::fromRgbF(1,1,1)));
 
   QTextLayouter textLayouter(&painter);
-  LabelLayouter<QGlyphRun,QTextLayouter> *layouter=&textLayouter.labelLayouter;
+  LabelLayouter<QGlyphRun,std::shared_ptr<QTextLayout>,QTextLayouter> *layouter=&textLayouter.labelLayouter;
 
   QString string="Hebrew Sarah (שרה) is spelled: sin (ש), resh (ר) and heh (ה)";
+  //QString string="A";
 
   int sinStart=0;
   for (int k=0;k<sinCount;k++){
@@ -276,10 +388,11 @@ void DrawWindow::paintEvent(QPaintEvent* /* event */)
       way.push_back(QPointF(x,y));
     }
     layouter->registerContourLabel(way, string.toStdString());
+    drawLine(&painter, way);
   }
 
-  for (const LabelInstance<QGlyphRun> &label:layouter->labels()){
-    for (const Glyph<QGlyphRun> &glyph:label.label.toGlyphs()){
+  for (const std::vector<Glyph<QGlyphRun>> &label:layouter->contourLabels()){
+    for (const Glyph<QGlyphRun> &glyph:label){
       drawGlyph(&painter, glyph);
     }
   }
@@ -303,7 +416,7 @@ void DrawWindow::paintEvent(QPaintEvent* /* event */)
 int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
-  DrawWindow window("",10);
+  DrawWindow window("",1);
   window.show();
 
   return app.exec();
