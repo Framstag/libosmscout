6d0b2f8bdeeb91a0af2795507ec933a4cfd5d710
Lukáš Karas <lukas.karas@centrum.cz>
2018-07-27 13:54:42 +0200

    create LabelPathTest

---
+++ Tests/src/LabelPathTest.cpp
@@ -0,0 +1,50 @@
+
+#include <osmscout/LabelPath.h>
+#include <osmscout/util/Geometry.h>
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+using namespace osmscout;
+
+TEST_CASE("Angle variance with low angle")
+{
+  LabelPath path;
+  path.AddPoint(0, 0);
+  path.AddPoint(10, 0);
+  path.AddPoint(20, 4);
+
+  REQUIRE(path.TestAngleVariance(0, 20, DegToRad(45)));
+  REQUIRE_FALSE(path.TestAngleVariance(0, 20, DegToRad(10)));
+}
+
+TEST_CASE("Angle variance with low angle 2")
+{
+  LabelPath path;
+  path.AddPoint(10, 0);
+  path.AddPoint(0, 0);
+  path.AddPoint(-10, 4);
+
+  REQUIRE(path.TestAngleVariance(0, 20, DegToRad(25)));
+  REQUIRE_FALSE(path.TestAngleVariance(0, 20, DegToRad(10)));
+}
+
+TEST_CASE("Angle variance with low angle 3")
+{
+  LabelPath path;
+  path.AddPoint(-10, 0);
+  path.AddPoint(0, 0);
+  path.AddPoint(10, 4);
+
+  REQUIRE(path.TestAngleVariance(0, 20, DegToRad(25)));
+  REQUIRE_FALSE(path.TestAngleVariance(0, 20, DegToRad(10)));
+}
+
+TEST_CASE("Angle variance with high angle")
+{
+  LabelPath path;
+  path.AddPoint(10,0);
+  path.AddPoint(0,0);
+  path.AddPoint(10,5);
+  REQUIRE_FALSE(path.TestAngleVariance(0, 20, DegToRad(45)));
+}
