4d9b3bfa674bd65c57e89d0730e3371431241df0
tteuling <tteuling@users.sourceforge.net>
2018-11-25 08:22:36 +0100

    Try to fix the TimeParse test.

--- Tests/src/TimeParse.cpp
+++ Tests/src/TimeParse.cpp
@@ -7,6 +7,6 @@ TEST_CASE("Parse ISO8601 time string") {
   osmscout::Timestamp ts;
   std::string testString="2017-03-12T14:31:56.0Z";
   REQUIRE(osmscout::ParseISO8601TimeString(testString, ts));
-  REQUIRE(ts.time_since_epoch().count()==1489329116000000000);
+  REQUIRE(std::chrono::duration_cast<std::chrono::milliseconds>(ts.time_since_epoch()).count()==1489329116000);
   REQUIRE(osmscout::TimestampToISO8601TimeString(ts)==testString);
 }
