079a091011b4266488050a6e8eee676f26a19b45
tteuling <tteuling@users.sourceforge.net>
2019-04-07 20:49:13 +0200

    Try to fix travis build
    * Renewed webpage upload data.
    * Updated hugo version.
    * Added web.sourceforge.net ssh key to known_hosts

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -9,6 +9,8 @@ echo "Compiler:   " $CXX
 
 echo "Installation start time: `date`"
 
+echo "web.sourceforge.net ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA2uifHZbNexw6cXbyg1JnzDitL5VhYs0E65Hk/tLAPmcmm5GuiGeUoI/B0eUSNFsbqzwgwrttjnzKMKiGLN5CWVmlN1IXGGAfLYsQwK6wAu7kYFzkqP4jcwc5Jr9UPRpJdYIK733tSEmzab4qc5Oq8izKQKIaxXNe7FgmL15HjSpatFt9w/ot/CHS78FUAr3j3RwekHCm/jhPeqhlMAgC+jUgNJbFt3DlhDaRMa0NYamVzmX8D47rtmBbEDU3ld6AezWBPUR5Lh7ODOwlfVI58NAf/aYNlmvl2TZiauBCTa7OPYSyXJnIPbQXg6YQlDknNCr0K769EjeIlAfY87Z4tw==" >> $HOME/.ssh/known_hosts
+
 export DEBIAN_FRONTEND=noninteractive
 
 if [ "$TARGET" = "build" ]; then
@@ -87,8 +89,8 @@ elif [ "$TARGET" = "importer" ]; then
 elif [ "$TARGET" = "website" ]; then
   echo "Installing dependencies for website..."
 
-  wget https://github.com/spf13/hugo/releases/download/v0.47/hugo_0.47_Linux-64bit.deb
-  sudo dpkg -i hugo_0.47_Linux-64bit.deb
+  wget https://github.com/gohugoio/hugo/releases/download/v0.54.0/hugo_0.54.0_Linux-64bit.deb
+  sudo dpkg -i hugo_0.54.0_Linux-64bit.deb
 
   sudo apt-get -qq update
   sudo apt-get install -y python3-pygments python-pygments doxygen lftp
