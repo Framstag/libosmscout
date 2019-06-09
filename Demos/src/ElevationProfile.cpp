d854e3c796141410a55da69b4ea61f0027d5e3c0
Lukáš Karas <lukas.karas@centrum.cz>
2019-06-09 17:56:16 +0200

    create ElevationService and ElevationProfile demo
    ElevationService is able to estimate way elevation profile
    using intersections with contour lines.
    
    Demo just loads track from gpx file and prints estimated
    elevation profile.

---
+++ Demos/src/ElevationProfile.cpp
@@ -0,0 +1,214 @@
+/*
+  GpxPipe - a demo program for libosmscout
+  Copyright (C) 2017 Lukas Karas
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include <osmscout/gpx/Import.h>
+
+#include <osmscout/util/CmdLineParsing.h>
+#include <osmscout/util/Logger.h>
+#include <osmscout/util/Distance.h>
+#include <osmscout/ElevationService.h>
+
+#include <iostream>
+#include <osmscout/Database.h>
+#include <osmscout/gpx/Export.h>
+
+struct Arguments
+{
+  bool        help{false};
+  std::string database;
+  std::string gpxInput;
+  std::string gpxOutput;
+};
+
+class DataLoader
+{
+private:
+  osmscout::DatabaseRef database;
+  osmscout::TypeInfoSet contourTypes;
+  osmscout::EleFeatureValueReader reader;
+  size_t count{0};
+  double millis{0};
+
+public:
+  DataLoader(osmscout::DatabaseRef &database):
+    database(database),
+    reader(*(database->GetTypeConfig()))
+  {
+    assert(database);
+
+    for (const auto &type:database->GetTypeConfig()->GetWayTypes()){
+      if (type->HasFeature(osmscout::EleFeature::NAME)){
+        osmscout::log.Debug() << "Using type " << type->GetName();
+        contourTypes.Set(type);
+      }
+    }
+  }
+
+  std::vector<osmscout::ContoursData> LoadContours(const osmscout::GeoBox &box)
+  {
+    osmscout::StopClock stopClock;
+    std::vector<osmscout::FileOffset> offsets;
+    osmscout::TypeInfoSet loadedTypes;
+    assert(database->GetAreaWayIndex()->GetOffsets(box,contourTypes,offsets,loadedTypes));
+    std::vector<osmscout::WayRef> contours;
+    assert(database->GetWaysByOffset(offsets, contours));
+    stopClock.Stop();
+    count++;
+    millis+=stopClock.GetMilliseconds();
+    return std::vector<osmscout::ContoursData>{osmscout::ContoursData{reader,contours}};
+  }
+
+  size_t GetCount() const
+  {
+    return count;
+  }
+
+  double GetMillis() const
+  {
+    return millis;
+  }
+};
+
+int main(int argc, char* argv[])
+{
+  osmscout::CmdLineParser   argParser("ElevationProfile",
+                                      argc,argv);
+  std::vector<std::string>  helpArgs{"h","help"};
+  Arguments                 args;
+
+  argParser.AddOption(osmscout::CmdLineFlag([&args](const bool& value) {
+                        args.help=value;
+                      }),
+                      helpArgs,
+                      "Return argument help",
+                      true);
+
+  argParser.AddOption(osmscout::CmdLineStringOption([&args](const std::string &s) {
+                        args.gpxOutput=s;
+                      }),
+                      "debugGpxOutput",
+                      "file for debug gpx output");
+
+  argParser.AddPositional(osmscout::CmdLineStringOption([&args](const std::string& value) {
+                            args.database=value;
+                          }),
+                          "DATABASE",
+                          "OSMScout database directory");
+
+  argParser.AddPositional(osmscout::CmdLineStringOption([&args](const std::string& value) {
+                            args.gpxInput=value;
+                          }),
+                          "GPXFILEINPUT",
+                          "Gpx file to analyse");
+
+  osmscout::CmdLineParseResult result=argParser.Parse();
+
+  if (result.HasError()) {
+    std::cerr << "ERROR: " << result.GetErrorDescription() << std::endl;
+    std::cout << argParser.GetHelp() << std::endl;
+    return 1;
+  } else if (!osmscout::ExistsInFilesystem(args.gpxInput)) {
+    std::cerr << "ERROR: Input file " << args.gpxInput << " don't exists" << std::endl;
+    std::cout << argParser.GetHelp() << std::endl;
+    return 1;
+  } else if (args.help) {
+    std::cout << argParser.GetHelp() << std::endl;
+    return 0;
+  }
+
+  try {
+    std::locale::global(std::locale(""));
+  }
+  catch (const std::runtime_error& e) {
+    std::cerr << "Cannot set locale: \"" << e.what() << "\"" << std::endl;
+  }
+
+  osmscout::log.Debug(true);
+
+  osmscout::gpx::GpxFile gpxFile;
+
+  if (!ImportGpx(args.gpxInput, gpxFile)){
+    return 1;
+  }
+  if (gpxFile.tracks.empty()){
+    std::cerr << "No track in gpx file" << std::endl;
+    return 1;
+  }
+  if (gpxFile.tracks[0].segments.empty()){
+    std::cerr << "No segments in first track" << std::endl;
+    return 1;
+  }
+
+  osmscout::StopClock filterStopClock;
+  gpxFile.tracks[0].FilterPoints([](std::vector<osmscout::gpx::TrackPoint> &points){
+    osmscout::gpx::FilterInaccuratePoints(points, 30);
+    osmscout::gpx::FilterNearPoints(points, osmscout::Distance::Of<osmscout::Meter>(10));
+  });
+  filterStopClock.Stop();
+  osmscout::log.Debug() << "Filter track: " << filterStopClock.ResultString();
+
+
+  // extract geo points from first segment
+  std::vector<osmscout::GeoCoord> way;
+  for (const auto &point:gpxFile.tracks[0].segments[0].points){
+    way.push_back(point.coord);
+  }
+
+  osmscout::DatabaseParameter databaseParameter;
+  osmscout::DatabaseRef       database=std::make_shared<osmscout::Database>(databaseParameter);
+
+  if (!database->Open(args.database)) {
+    std::cerr << "Cannot open database" << std::endl;
+    return 1;
+  }
+
+  osmscout::StopClock stopClock;
+
+  DataLoader dataLoader(database);
+  osmscout::ElevationService<DataLoader> eleService(dataLoader, osmscout::Magnification::magSuburb);
+  auto profile=eleService.ElevationProfile(way);
+
+  stopClock.Stop();
+  osmscout::log.Debug() << "Evaluating elevation profile: " << stopClock.ResultString() << " (" << dataLoader.GetCount() << "x loading: " << dataLoader.GetMillis()/1000 << " s)";
+
+  if (profile.empty()){
+    std::cout << "No intersection with contours found." << std::endl;
+    return 0;
+  }
+  std::cout << "Found " << profile.size() << " intersections" << std::endl;
+
+  std::cout << "Distance \tElevation \tCoord (type)" << std::endl;
+  for (const auto &point: profile) {
+    std::cout << point.distance << " \t" << point.elevation.AsMeter() << " m \t" << point.coord.GetDisplayText() << " (" << point.contour->GetType()->GetName() << " " << point.contour->GetFileOffset() << ")" << std::endl;
+  }
+
+  // export debug gpx output
+  if (!args.gpxOutput.empty()) {
+    osmscout::gpx::GpxFile output;
+    output.tracks.push_back(gpxFile.tracks[0]);
+    for (const auto &point: profile) {
+      osmscout::gpx::Waypoint w(point.coord);
+      w.name = osmscout::gpx::Optional<std::string>::of(point.distance.AsString() + " " + std::to_string(point.elevation.AsMeter()) + " m");
+      output.waypoints.push_back(w);
+    }
+    osmscout::gpx::ExportGpx(output, args.gpxOutput);
+  }
+
+  return 0;
+}
---
+++ libosmscout/include/osmscout/ElevationService.h
@@ -0,0 +1,138 @@
+#ifndef OSMSCOUT_ELEVATION_SERVICE_H
+#define OSMSCOUT_ELEVATION_SERVICE_H
+
+/*
+  This source is part of the libosmscout library
+  Copyright (C) 2019  Lukas Karas
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+*/
+
+#include <osmscout/TypeConfig.h>
+#include <osmscout/Way.h>
+#include <osmscout/CoreImportExport.h>
+#include <osmscout/FeatureReader.h>
+
+#include <osmscout/util/TileId.h>
+
+#include <vector>
+
+namespace osmscout {
+
+struct ContoursData
+{
+  EleFeatureValueReader reader;
+  std::vector<WayRef> contours;
+};
+
+struct ElevationPoint
+{
+  Distance distance;
+  Distance elevation;
+  GeoCoord coord;
+  WayRef contour;
+};
+
+template <typename DataLoader>
+class ElevationService CLASS_FINAL
+{
+private:
+  DataLoader &dataLoader;
+  MagnificationLevel loadTileMag;
+
+public:
+  ElevationService(DataLoader &dataLoader,
+                   MagnificationLevel loadTileMag = Magnification::magSuburb):
+      dataLoader(dataLoader), loadTileMag(loadTileMag)
+  {}
+
+  std::vector<ElevationPoint> ElevationProfile(const std::vector<GeoCoord> &way)
+  {
+    std::vector<ElevationPoint> result;
+    Distance distance;
+    GeoCoord intersection;
+
+    std::vector<ContoursData> contours;
+    GeoBox loadBox;
+
+    for (size_t i=0; i < way.size()-1; i+=1){
+      GeoCoord a1=way[i];
+      GeoCoord a2=way[i+1];
+      GeoBox lineBox(a1,a2);
+
+      if (!loadBox.Includes(a1) || !loadBox.Includes(a2)) {
+        TileId tile1=TileId::GetTile(loadTileMag, lineBox.GetMinCoord());
+        TileId tile2=TileId::GetTile(loadTileMag, lineBox.GetMaxCoord());
+        TileIdBox tileBox(tile1, tile2);
+        loadBox=tileBox.GetBoundingBox(Magnification(loadTileMag));
+        if (tileBox.GetCount() > 100){
+          osmscout::log.Warn() << "Too huge area for loading " << loadBox.GetDisplayText();
+          continue;
+        }
+        //osmscout::log.Debug() << "box " << lineBox.GetDisplayText() << " -> " << loadBox.GetDisplayText() << " size: " << tileBox.GetCount();
+        assert(loadBox.Includes(a1));
+        assert(loadBox.Includes(a2));
+        contours = dataLoader.LoadContours(loadBox);
+      }
+
+      for (const ContoursData &contoursData:contours) {
+
+        for (const auto &contour:contoursData.contours) {
+          if (!lineBox.Intersects(contour->GetBoundingBox())){
+            continue;
+          }
+
+          EleFeatureValue *eleValue=contoursData.reader.GetValue(contour->GetFeatureValueBuffer());
+          if (!eleValue) {
+            continue;
+          }
+
+          for (size_t bi=0; bi < contour->nodes.size()-1; bi+=1){
+            GeoCoord b1=contour->nodes[bi].GetCoord();
+            GeoCoord b2=contour->nodes[bi+1].GetCoord();
+            if (GetLineIntersection(a1,a2,
+                                    b1,b2,
+                                    intersection)) {
+
+              result.push_back(ElevationPoint{
+                                   distance + GetEllipsoidalDistance(a1, intersection),
+                                   Meters(eleValue->GetEle()),
+                                   intersection,
+                                   contour
+                               });
+
+              //log.Debug() << "  " << eleValue->GetEle() << " m, distance " << distance << " + " << GetEllipsoidalDistance(a1, intersection) << " : " << intersection.GetDisplayText();
+            }
+          }
+
+        }
+      }
+      //log.Debug() << "At distance " << distance << " adding " << GetEllipsoidalDistance(a1,a2) << " (" << a1.GetDisplayText() << " -> " << a2.GetDisplayText() << ")";
+      distance+=GetEllipsoidalDistance(a1,a2);
+    }
+    std::sort(result.begin(), result.end(), [](const ElevationPoint &a, const ElevationPoint &b) {
+      if (a.distance != b.distance)
+        return a.distance < b.distance;
+      if (a.elevation != b.elevation)
+        return a.elevation < b.elevation;
+      return a.coord < b.coord;
+    });
+    return result;
+  }
+};
+
+}
+
+#endif //OSMSCOUT_ELEVATION_SERVICE_H
