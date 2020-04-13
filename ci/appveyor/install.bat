b0789ed1089934c7b920b1d7785222bd15d3a1c8
Tim Teulings <Framstag@users.noreply.github.com>
2020-04-13 13:53:53 +0200

    Improved syntax, added debugging

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -61,235 +61,235 @@ IF %COMPILER%==msys2 (
 )
 
 IF %COMPILER%==msvc2019 (
-  @echo on
+  rem @echo on
   echo MSVC2019 build...
 
-  set starttime=%time%
+  set timestart=%time%
 
   IF %BUILDTOOL%==cmake (
     cd c:\tools\vcpkg
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing zlib, iconv, libxml2...
       .\vcpkg install zlib:x64-windows libiconv:x64-windows libxml2:x64-windows
     )
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing protobuf...
       .\vcpkg install protobuf:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing pixman, expat, fontconfig, cairo, harfbuzz, pango...
       .\vcpkg install pixman:x64-windows expat:x64-windows fontconfig:x64-windows cairo:x64-windows harfbuzz:x64-windows pango:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing double-conversion...
       .\vcpkg install double-conversion:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-modularscripts...
       .\vcpkg install qt5-modularscripts:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing openssl-windows, openssl...
       .\vcpkg install openssl-windows:x64-windows openssl:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing libpq...
       .\vcpkg install libpq:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing sqlite3...
       .\vcpkg install sqlite3:x64-windows
     )
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-base...
       .\vcpkg install qt5-base:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-graphicaleffects...
       .\vcpkg install qt5-graphicaleffects:x64-windows
     )
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-declarative...
       .\vcpkg install qt5-declarative:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-quickcontrols...
       .\vcpkg install qt5-quickcontrols:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-speech...
       .\vcpkg install qt5-speech:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-charts...
       .\vcpkg install qt5-charts:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-datavis3d...
       .\vcpkg install qt5-datavis3d:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-multimedia...
       .\vcpkg install qt5-multimedia:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-3d...
       .\vcpkg install qt5-3d:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-gamepad...
       .\vcpkg install qt5-gamepad:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-imageformats...
       .\vcpkg install qt5-imageformats:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-networkauth...
       .\vcpkg install qt5-networkauth:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-quickcontrols2...
       .\vcpkg install qt5-quickcontrols2:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-scxml...
       .\vcpkg install qt5-scxml:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-serialport...
       .\vcpkg install qt5-serialport:x64-windows
     )
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-svg...
       .\vcpkg install qt5-svg:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-virtualkeyboard...
       .\vcpkg install qt5-virtualkeyboard:x64-windows
     )
 
     rem transitive
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-websockets...
       .\vcpkg install qt5-websockets:x64-windows
     )
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5...
       .\vcpkg install qt5:x64-windows
     )
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing qt5-tools...
       .\vcpkg install qt5-tools:x64-windows
     )
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing OpenGL...
       .\vcpkg install opengl:x64-windows
     )
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing freeglut...
       .\vcpkg install freeglut:x64-windows
     )
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing glm...
       .\vcpkg install glm:x64-windows
     )
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing glew...
       .\vcpkg install glew:x64-windows
     )
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo Installing glfw3...
       .\vcpkg install glfw3:x64-windows
     )
 
-    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %starttime%
-    if %timediff% LEQ 1800 (
+    %APPVEYOR_BUILD_FOLDER%\ci\timediff.cmd %timestart%
+    IF %timediff% LEQ 1800 (
       echo System-wide integrating vcpkg...
       .\vcpkg integrate install
     )
