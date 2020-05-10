fb10fd7f179e322c784a85319a1e792d641e2085
tteuling <tteuling@users.sourceforge.net>
2020-05-10 17:29:23 +0200

    Replace typedef with using

--- libosmscout-gpx/include/osmscout/gpx/Utils.h
+++ libosmscout-gpx/include/osmscout/gpx/Utils.h
@@ -26,12 +26,11 @@
 #include <memory>
 #include <vector>
 
-namespace osmscout {
-namespace gpx {
+namespace osmscout::gpx {
 
 class OSMSCOUT_GPX_API ProcessCallback {
 public:
-  virtual ~ProcessCallback(){};
+  virtual ~ProcessCallback() = default;
 
   /**
    * Callback for reporting import progress
@@ -46,7 +45,7 @@ public:
   virtual void Error(const std::string& error);
 };
 
-typedef std::shared_ptr<ProcessCallback> ProcessCallbackRef;
+using ProcessCallbackRef = std::shared_ptr<ProcessCallback>;
 
 /**
  * Filter out following points that are not distant more than minDistance.
@@ -68,7 +67,6 @@ extern OSMSCOUT_GPX_API void FilterNearPoints(std::vector<TrackPoint> &points,
 extern OSMSCOUT_GPX_API void FilterInaccuratePoints(std::vector<TrackPoint> &points,
                                                     double maxDilution=30);
 
-}
 }
 
 #endif //LIBOSMSCOUT_GPX_UTILS_H
