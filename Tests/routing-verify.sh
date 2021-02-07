e472a94f4cabb60c693e318b597922011665aa17
Lukáš Karas <lukas.karas@centrum.cz>
2021-02-07 14:36:37 +0100

    don't fail after first error

--- Tests/routing-verify.sh
+++ Tests/routing-verify.sh
@@ -28,16 +28,19 @@ if [ ! -f "${GERMAN_DATABASE}/types.dat" ] ; then
   exit 1
 fi
 
+FAILURES=0
+
 function verifyRouting() {
   REFERENCE_JSON="$1"
-  echo "Verifying route $(basename --suffix='.json' "$REFERENCE_JSON")"
+  echo -n "Verifying route $(basename --suffix='.json' "$REFERENCE_JSON")... "
   shift
   TEST_OUTPUT=$(mktemp --suffix='.json')
   "$ROUTING_DEMO" --routeJson "$TEST_OUTPUT" "$@" > /dev/null || exit 1
   if [ $(jq --argfile reference "$REFERENCE_JSON" --argfile test "$TEST_OUTPUT" -n '$reference == $test') != "true" ] ; then
-    echo "Route don't match to $REFERENCE_JSON" >&2
-    rm "$TEST_OUTPUT"
-    exit 1
+    echo "Route don't match to $REFERENCE_JSON"
+    FAILURES=$(( $FAILURES + 1 ))
+  else
+    echo "Match"
   fi
   rm "$TEST_OUTPUT"
 }
@@ -46,4 +49,5 @@ verifyRouting "${TOP_DIR}/Tests/data/routing/srbsko.json"          "${CZECH_DATA
 verifyRouting "${TOP_DIR}/Tests/data/routing/harrachov-praha.json" "${CZECH_DATABASE}" 50.7728 15.4261 50.1004 14.4453
 verifyRouting "${TOP_DIR}/Tests/data/routing/sumburk.json"         "${CZECH_DATABASE}" 50.7509 15.3151 50.7335 15.3000
 
-echo "Done"
+echo "$FAILURES issues"
+exit $FAILURES
