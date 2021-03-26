4e3c56316717a24caef2899df777c7391b6a2540
Vladimir Vyskocil <vladimir.vyskocil@gmail.com>
2021-03-26 21:51:29 +0100

    Keep sub directory structure for OSMScout framework headers Embed cfref_ptr.hpp include in MapPainterIOS.h

--- libosmscout-map-iosx/include/osmscout/MapPainterIOS.h
+++ libosmscout-map-iosx/include/osmscout/MapPainterIOS.h
@@ -25,6 +25,7 @@
 
 #include <string>
 #include <unordered_map>
+#include <utility>
 
 #if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
@@ -39,7 +40,91 @@
 
 #import <CoreText/CoreText.h>
 #include <osmscout/MapPainter.h>
-#include <osmscout/private/cfref_ptr.hpp>
+
+template<class T>
+class cfref_ptr
+{
+public:
+    cfref_ptr()
+    : obj_(NULL)
+    {}
+    
+    cfref_ptr(T obj)
+    : obj_(obj)
+    {
+    }
+    
+    ~cfref_ptr()
+    {
+        if (obj_) {
+            CFRelease(obj_);
+        }
+    }
+    
+    cfref_ptr(cfref_ptr const& other)
+    : obj_(other.obj_)
+    {
+        if (obj_) {
+            CFRetain(obj_);
+        }
+    }
+    
+    cfref_ptr(cfref_ptr&& other) noexcept
+    {
+        *this = std::move(other);
+    }
+    
+    cfref_ptr& operator = (cfref_ptr const& other)
+    {
+        if (this != &other) {
+            cfref_ptr tmp(other);
+            tmp.swap(*this);
+        }
+        return *this;
+    }
+    
+    cfref_ptr& operator = (cfref_ptr&& other)
+    {
+        if (this != &other) {
+            obj_ = other.obj_;
+            other.obj_ = NULL;
+        }
+        return *this;
+    }
+    
+    bool boolean_test() const
+    {
+        return !!obj_;
+    }
+    
+    T get()
+    {
+        return obj_;
+    }
+    
+    T release()
+    {
+        T ret = obj_;
+        obj_ = NULL;
+        return ret;
+    }
+    
+    void swap(cfref_ptr& other)
+    {
+        std::swap(obj_, other.obj_);
+    }
+    
+    void reset(T obj = NULL)
+    {
+        if (obj_) {
+            CFRelease(obj_);
+        }
+        obj_ = obj;
+    }
+    
+private:
+    T obj_;
+};
 
 namespace osmscout {
     struct IOSGlyphInRun {
