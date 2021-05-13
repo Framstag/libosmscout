6da2242977585572c641a51e1463fd547d0caff9
tteuling <tteuling@users.sourceforge.net>
2021-05-13 20:02:57 +0200

    Remove webpage upload from Travis-CI

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -9,8 +9,6 @@ echo "Compiler:   " "$CXX"
 
 echo "Installation start time: $(date)"
 
-echo "web.sourceforge.net ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA2uifHZbNexw6cXbyg1JnzDitL5VhYs0E65Hk/tLAPmcmm5GuiGeUoI/B0eUSNFsbqzwgwrttjnzKMKiGLN5CWVmlN1IXGGAfLYsQwK6wAu7kYFzkqP4jcwc5Jr9UPRpJdYIK733tSEmzab4qc5Oq8izKQKIaxXNe7FgmL15HjSpatFt9w/ot/CHS78FUAr3j3RwekHCm/jhPeqhlMAgC+jUgNJbFt3DlhDaRMa0NYamVzmX8D47rtmBbEDU3ld6AezWBPUR5Lh7ODOwlfVI58NAf/aYNlmvl2TZiauBCTa7OPYSyXJnIPbQXg6YQlDknNCr0K769EjeIlAfY87Z4tw==" >> "$HOME"/.ssh/known_hosts
-
 export DEBIAN_FRONTEND=noninteractive
 
 if [ "$TARGET" = "importer" ]; then
@@ -29,14 +27,6 @@ if [ "$TARGET" = "importer" ]; then
       libicu-dev \
       liblzma-dev
   fi
-elif [ "$TARGET" = "website" ]; then
-  echo "Installing dependencies for website..."
-
-  wget https://github.com/gohugoio/hugo/releases/download/v0.76.5/hugo_0.76.5_Linux-64bit.deb
-  sudo dpkg -i hugo_0.76.5_Linux-64bit.deb
-
-  sudo apt-get -qq update
-  sudo apt-get install -y python3-pygments python-pygments doxygen lftp
 fi
 
 echo "Installation end time: $(date)"
