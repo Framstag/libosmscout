46cc8ee5d77da3b0fc797c669f2c25451d7845d6
tteuling <tteuling@users.sourceforge.net>
2021-05-13 11:34:19 +0200

    Remove meson code

--- ci/appveyor/test.sh
+++ ci/appveyor/test.sh
@@ -9,9 +9,5 @@ echo "BUILDTOOL: ${BUILDTOOL}"
 export LANG=en_US.utf8
 
 if [ "${BUILDTOOL}" = "cmake" ] ; then
-    cd build
-    ctest
-fi
-if [ "${BUILDTOOL}" = "meson" ] ; then
-  meson test -C debug --print-errorlogs
+    (cd build && ctest -j 2 --output-on-failure)
 fi
