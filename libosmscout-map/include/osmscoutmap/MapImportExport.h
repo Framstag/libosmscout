fbee2f285e8a7e1e8101d866a6a179bb42cd9e1e
tteuling <tteuling@users.sourceforge.net>
2021-10-11 11:38:45 +0200

    Fix *.h.cmake handling

--- libosmscout-map/include/osmscoutmap/MapImportExport.h
+++ libosmscout-map/include/osmscoutmap/MapImportExport.h
@@ -20,7 +20,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 */
 
-#include <osmscoutmap/MapFeatures.h.cmake>
+#include <osmscoutmap/MapFeatures.h>
 
 // Shared library support
 #if defined _WIN32 || defined __CYGWIN__
