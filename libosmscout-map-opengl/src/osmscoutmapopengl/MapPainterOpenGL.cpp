4270f719c8a1f644e69ef6eaad713d77be86ffd3
Lukáš Karas <lukas.karas@centrum.cz>
2021-12-28 15:06:12 +0100

    missing braces

--- libosmscout-map-opengl/src/osmscoutmapopengl/MapPainterOpenGL.cpp
+++ libosmscout-map-opengl/src/osmscoutmapopengl/MapPainterOpenGL.cpp
@@ -1094,8 +1094,9 @@ namespace osmscout {
           continue;
         }
 
-        if (hasIcon)
+        if (hasIcon) {
           offset = 15;
+        }
 
         double alpha = 1.0;
         double fontSize = 1.0;
