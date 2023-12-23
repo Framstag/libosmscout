ddd5c98e33feb58c3f863dc538a1a82acce14c2d
Lukáš Karas <lukas.karas@centrum.cz>
2023-12-23 17:42:44 +0100

    locale-aware byte size formatter
    Char-based number formatting (using std::stringstream)
    is broken for locales with non-ascii thousand separator
    (at least with gcc 8.3.0), for that reason, we should
    provide custom locale aware number-to-string conversion
    for float numbers and use it in ByteSizeToString utility.

--- libosmscout/src/osmscout/util/String.cpp
+++ libosmscout/src/osmscout/util/String.cpp
@@ -196,6 +196,7 @@ namespace osmscout {
   std::string NumberToString(long value, const Locale &locale)
   {
     std::stringstream ss;
+    ss.imbue(std::locale("C"));
     if (std::abs(value) < 1000 || locale.GetThousandsSeparator().empty()){
       ss << value;
     }else{
@@ -209,6 +210,27 @@ namespace osmscout {
         value = std::abs(value % mag);
         mag/=1000;
         ss << std::setw(3) << std::setfill('0') << (value/mag);
+      }
+    }
+    return ss.str();
+  }
+
+  extern OSMSCOUT_API std::string FloatToString(double value, const Locale &locale, uint32_t precision)
+  {
+    std::stringstream ss;
+    double order = std::pow(10, precision);
+    value = std::round(value * order) / order;
+    ss << NumberToString(static_cast<long>(value), locale);
+
+    if (precision > 0) {
+      ss << locale.GetDecimalSeparator();
+      double fractionNum = std::abs(value) - std::floor(std::abs(value));
+      std::string fraction = NumberToString(fractionNum * order, Locale());
+      for (size_t i = 0; i < fraction.size(); i++) {
+        if (i > 0 && i % 3 == 0 && i < fraction.size() - 1) {
+          ss << locale.GetThousandsSeparator();
+        }
+        ss << fraction[i];
       }
     }
     return ss.str();
@@ -329,36 +351,32 @@ namespace osmscout {
     return wordCount;
   }
 
-  std::string ByteSizeToString(FileOffset value)
+  std::string ByteSizeToString(FileOffset value, const Locale &locale)
   {
-    return ByteSizeToString((double)value);
+    return ByteSizeToString(static_cast<double>(value), locale);
   }
 
-  std::string ByteSizeToString(double value)
+  std::string ByteSizeToString(double value, const Locale &locale)
   {
     std::stringstream buffer;
 
-    buffer.setf(std::ios::fixed);
-    buffer << std::setprecision(1);
-
     if (value<1.0 && value>-1) {
-      buffer << "0 B";
+      buffer << "0" << locale.GetUnitsSeparator() << "B";
     }
-    else if (ceil(value)>=1024.0*1024*1024*1024) {
-      buffer << value/(1024.0*1024*1024*1024) << " TiB";
+    else if (ceil(value)>=std::pow(1024.0, 4.0)) {
+      buffer << FloatToString(value/std::pow(1024.0, 4.0), locale, 1) << locale.GetUnitsSeparator()  << "TiB";
     }
-    else if (ceil(value)>=1024.0*1024*1024) {
-      buffer << value/(1024.0*1024*1024) << " GiB";
+    else if (ceil(value)>=std::pow(1024.0, 3.0)) {
+      buffer << FloatToString(value/std::pow(1024.0, 3.0), locale, 1) << locale.GetUnitsSeparator() << "GiB";
     }
-    else if (ceil(value)>=1024.0*1024) {
-      buffer << value/(1024.0*1024) << " MiB";
+    else if (ceil(value)>=std::pow(1024.0, 2.0)) {
+      buffer << FloatToString(value/std::pow(1024.0, 2.0), locale, 1) << locale.GetUnitsSeparator() << "MiB";
     }
     else if (ceil(value)>=1024.0) {
-      buffer << value/1024.0 << " KiB";
+      buffer << FloatToString(value/1024.0, locale, 1) << locale.GetUnitsSeparator() << "KiB";
     }
     else {
-      buffer << std::setprecision(0);
-      buffer << value << " B";
+      buffer << FloatToString(value, locale, 0) << locale.GetUnitsSeparator() << "B";
     }
 
     return buffer.str();
