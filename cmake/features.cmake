504911bcee82a0ebc7e49f7c719b24df2911226b
Lukáš Karas <lukas.karas@centrum.cz>
2023-12-24 12:09:42 +0100

    migrate from deprecated mallinfo to maillinfo2

--- cmake/features.cmake
+++ cmake/features.cmake
@@ -132,7 +132,7 @@ check_function_exists(fseeko HAVE_FSEEKO)
 check_function_exists(mmap HAVE_MMAP)
 check_function_exists(posix_fadvise HAVE_POSIX_FADVISE)
 check_function_exists(posix_madvise HAVE_POSIX_MADVISE)
-check_function_exists(mallinfo HAVE_MALLINFO)
+check_function_exists(mallinfo2 HAVE_MALLINFO2)
 
 # prefer static libraries if shared are disabled
 if(NOT BUILD_SHARED_LIBS AND (CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANGXX OR CMAKE_COMPILER_IS_GNUCC))
