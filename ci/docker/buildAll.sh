98aacb669b98232b01c1a7f92543b46cdc0f2844
Lukáš Karas <lukas.karas@centrum.cz>
2025-08-03 01:10:33 +0200

    add docker build for Debian Trixie

--- ci/docker/buildAll.sh
+++ ci/docker/buildAll.sh
@@ -8,6 +8,7 @@ set -xe
 ./debian_buster_gcc_meson/build.sh
 ./debian_bullseye_gcc_meson/build.sh
 ./debian_bookworm_gcc_meson/build.sh
+./debian_13_trixie_gcc_meson/build.sh
 ./ubuntu_22.04_gcc_cmake/build.sh
 ./ubuntu_22.04_gcc_meson_qt6/build.sh
 ./ubuntu_22.04_aarch64_gcc_cmake/build.sh
